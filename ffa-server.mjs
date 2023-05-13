ec52861eddee80072d60792b827a8c5b723bad29
cs6413110 <129222222+cs6413110@users.noreply.github.com>
2023-05-12 21:14:33 -0700

    Update ffa-server.mjs

--- ffa-server.mjs
+++ ffa-server.mjs
@@ -133,8 +133,8 @@ Core.ws(SETTINGS.path, {idleTimeout: Infinity, max_backpressure: 1}, (socket) =>
       }
       servers[socket.room].logs.push({m: '['+socket.username+'] '+msg, c: '#ffffff'});
     } else if (data.type === 'command') {
-      if (typeof Comands[data.data[0].replace('/', '')] === 'function') {
-        Comands[data.data[0].replace('/', '')].bind(socket)(data.data);
+      if (typeof Commands[data.data[0].replace('/', '')] === 'function') {
+        Commands[data.data[0].replace('/', '')].bind(socket)(data.data);
       } else return socket.send({status: 'error', message: 'Command not found.'});
     } else if (data.type === 'stats') {
       var players = [];
@@ -146,7 +146,7 @@ Core.ws(SETTINGS.path, {idleTimeout: Infinity, max_backpressure: 1}, (socket) =>
     if (socket.room !== undefined) servers[socket.room].disconnect(socket, code, reason);
   });
 });
-const Comands = {
+const Commands = {
   createteam: data => {
     if (data.length !== 2) return this.send({status: 'error', message: 'Command has invalid arguments.'});
     if (servers[this.room].pt.find(t => servers[this.room].getTeam(t.team) === data[1])) return this.send({status: 'error', message: 'This team already exists.'});
