587f85f542a70203311d37d5ae76be3ac3f39d24
cs6413110 <129222222+cs6413110@users.noreply.github.com>
2023-06-21 17:34:36 -0700

    bruh it deleted my entire encoding method

--- main.mjs
+++ main.mjs
@@ -13,7 +13,8 @@ const HyperExpressServer = new Server({fast_buffers: true}), router = new Router
 let tokens = new Set(), sockets = [], db;
 
 const valid = (token, username) => tokens.has(`${token}:${username}`);
-
+const encode = (c) => {var x='charCodeAt',b,e={},f=c.split(""),d=[],a=f[0],g=256;for(b=1;b<f.length;b++)c=f[b],null!=e[a+c]?a+=c:(d.push(1<a.length?e[a]:a[x](0)),e[a+c]=g,g++,a=c);d.push(1<a.length?e[a]:a[x](0));for(b=0;b<d.length;b++)d[b]=String.fromCharCode(d[b]);return d.join("")}
+const decode = (b) => {var a,e={},d=b.split(""),c=d[0],f=d[0],g=[c],h=256,o=256;for(b=1;b<d.length;b++)a=d[b].charCodeAt(0),a=h>a?d[b]:e[a]?e[a]:f+c,g.push(a),c=a.charAt(0),e[o]=f+c,o++,f=a;return g.join("")}
 const routes = {
   auth: async ({ username, type, password }, socket) => {
     if (['', ' ', undefined].includes(username) || username.includes(' ') || username.includes(':')) return socket.send({ status: 'error', message: 'Invalid username.'});
