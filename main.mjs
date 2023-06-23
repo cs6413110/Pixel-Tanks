138d2acd290f2c20d3a4233d30b11f47f4773ebb
cs6413110 <129222222+cs6413110@users.noreply.github.com>
2023-06-22 18:19:47 -0700

    idk

--- main.mjs
+++ main.mjs
@@ -9,16 +9,17 @@ import {Core} from './ffa-server.mjs';
 const connectionString =
   'mongodb+srv://cs641311:355608-G38@cluster0.z6wsn.mongodb.net/?retryWrites=true&w=majority', port = 80;
 
-const HyperExpressServer = new Server({fast_buffers: true}), router = new Router(), client = new MongoClient(connectionString), filter = new Filter(), tokgen = new TokenGenerator(256, TokenGenerator.BASE62);
-let tokens = new Set(), sockets = [], db;
+const HyperExpressServer = new Server({fast_buffers: true}), router = new Router(), client = new MongoClient(connectionString), filter = new Filter(), tokgen = new TokenGenerator(256, TokenGenerator.BASE62), tokens = new Set(), sockets = [];
+let db;
+
 (async () => {
   await client.connect();
   db = client.db('data').collection('data');
 })();
 
 const valid = (token, username) => tokens.has(`${token}:${username}`);
-const encode = (c) => {var x='charCodeAt',b,e={},f=c.split(""),d=[],a=f[0],g=256;for(b=1;b<f.length;b++)c=f[b],null!=e[a+c]?a+=c:(d.push(1<a.length?e[a]:a[x](0)),e[a+c]=g,g++,a=c);d.push(1<a.length?e[a]:a[x](0));for(b=0;b<d.length;b++)d[b]=String.fromCharCode(d[b]);return d.join("")}
-const decode = (b) => {var a,e={},d=b.split(""),c=d[0],f=d[0],g=[c],h=256,o=256;for(b=1;b<d.length;b++)a=d[b].charCodeAt(0),a=h>a?d[b]:e[a]?e[a]:f+c,g.push(a),c=a.charAt(0),e[o]=f+c,o++,f=a;return g.join("")}
+const encode = (c) => {let x='charCodeAt',b,e={},f=c.split(""),d=[],a=f[0],g=256;for(b=1;b<f.length;b++)c=f[b],null!=e[a+c]?a+=c:(d.push(1<a.length?e[a]:a[x](0)),e[a+c]=g,g++,a=c);d.push(1<a.length?e[a]:a[x](0));for(b=0;b<d.length;b++)d[b]=String.fromCharCode(d[b]);return d.join("")}
+const decode = (b) => {let a,e={},d=b.split(""),c=d[0],f=d[0],g=[c],h=256,o=256;for(b=1;b<d.length;b++)a=d[b].charCodeAt(0),a=h>a?d[b]:e[a]?e[a]:f+c,g.push(a),c=a.charAt(0),e[o]=f+c,o++,f=a;return g.join("")}
 const routes = {
   auth: async ({ username, type, password }, socket) => {
     if (['', ' ', undefined].includes(username) || username.includes(' ') || username.includes(':')) return socket.send({ status: 'error', message: 'Invalid username.'});
@@ -70,7 +71,7 @@ router.ws('/', {idle_timeout: Infinity}, (socket) => {
   });
 });
 
-HyperExpressServer.get('/verify', (req, res) => res.end(valid(req.query.token, req.query.username).toString()));
+HyperExpressServer.get('/verify', (req, res) => res.end(valid(req.query.token, req.query.username)));
 HyperExpressServer.get('/*', async(req, res) => {
   res.header('Content-Type', 'application/javascript').end(await fs.readFile('/home/ubuntu/Pixel-Tanks/public/js/pixel-tanks.js'));
 });
