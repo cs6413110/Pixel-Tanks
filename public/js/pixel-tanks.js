4921e575fdbf976119f837d735c0dfd07c2e4d57
Aaron Godfrey <3foes@mail.com>
2023-07-10 18:52:41 -0700

    Menu

--- public/js/pixel-tanks.js
+++ public/js/pixel-tanks.js
@@ -734,7 +734,7 @@
           }
         }
         const data = GUI.draw.getImageData(b[0]/1600*700, b[1]/1000*438, b[2]/1600*700, b[3]/1000*438);
-        GUI.draw.putImageData(b[0]-b[6], b[1]-b[6], b[2]+b[6]*2, b[3]+b[6]*2);
+        GUI.draw.putImageData(data, b[0]-b[6], b[1]-b[6], b[2]+b[6]*2, b[3]+b[6]*2);
       }
     }
   }
