57ef3917f2080348d71fca78a1d4a3f60bbc877b
Aaron Godfrey <3foes@mail.com>
2023-07-17 20:24:42 -0700

    fix

--- public/js/engine.js
+++ public/js/engine.js
@@ -117,7 +117,7 @@ class Engine {
     }
     if (use.includes('turret')) {
       this.ai.splice(this.ai.indexOf(this.ai.find(a => this.getUsername(a.team) === t.username)), 1);
-      this.ai.push(new AI(Math.floor(t.x/100)+10, Math.floor(t.y/100)+10, 1, t.rank, t.team, this));
+      this.ai.push(new AI(Math.floor(t.x/100)*100+10, Math.floor(t.y/100)*100+10, 1, t.rank, t.team, this));
     }
     if (use.includes('buff')) {
       t.buff = true;
