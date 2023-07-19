ecc8681f02a71b6a335faabc26f7319580a6dd51
Aaron Godfrey <3foes@mail.com>
2023-07-18 20:36:47 -0700

    a

--- public/js/engine.js
+++ public/js/engine.js
@@ -549,9 +549,9 @@ class AI {
       return;
     }
     console.log(frames);
-    const f = frames%25 === 0 ? Math.floor(frames/25)-1 : Math.floor(frames/25);
-    const dirx = this.path.p[f][0]-this.path.p[f+1][0] < 0 ? 1 : -1;
-    const diry = this.path.p[f][1]-this.path.p[f+1][1] < 0 ? 1 : -1;
+    const f = Math.floor(frames/25), n = f+1 === this.path.length ? f : f+1;
+    const dirx = this.path.p[f][0]-this.path.p[n][0] < 0 ? 1 : -1;
+    const diry = this.path.p[f][1]-this.path.p[n][1] < 0 ? 1 : -1;
     this.x = this.path.p[f][0]*100+10 + dirx * 4 * (frames%25);
     this.y = this.path.p[f][1]*100+10 + diry * 4 * (frames%25);
   }
