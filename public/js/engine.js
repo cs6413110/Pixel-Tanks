98dcb8ef26a4a3dcb8f6c29891486b7489cb09fc
cs6413110 <129222222+cs6413110@users.noreply.github.com>
2023-07-25 08:05:24 -0700

    Update engine.js

--- public/js/engine.js
+++ public/js/engine.js
@@ -118,7 +118,7 @@ class Engine {
     }
     if (use.includes('turret')) {
       this.ai.splice(this.ai.indexOf(this.ai.find(a => this.getUsername(a.team) === t.username)), 1);
-      this.ai.push(new AI(Math.floor(t.x / 100) * 100 + 10, Math.floor(t.y / 100) * 100 + 10, 2, t.rank, t.team+'L', this));
+      this.ai.push(new AI(Math.floor(t.x / 100) * 100 + 10, Math.floor(t.y / 100) * 100 + 10, 1, t.rank, t.team+'L', this));
     }
     if (use.includes('buff')) {
       t.buff = true;
