50b43710c57b8a4922e379a4de93f2ffad1b5462
cs6413110 <129222222+cs6413110@users.noreply.github.com>
2023-07-24 20:40:34 -0700

    more logging for debug

--- public/js/engine.js
+++ public/js/engine.js
@@ -619,7 +619,8 @@ class AI {
       limiter = [3, 4];
     } else limiter = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
     sortAsc = mode !== 2;
-    this.host.logs.push({m: 'c='+Math.abs(coords[0][1])+' m='+this.mode+' es='+(epx === sx && epy === sy)+' t='+Date.now(), c: '#ffffff'});
+    this.host.logs.push({m: 'gen: ', c: '#ffffff'});
+    this.host.logs.push({m: 'c='+coords[0][1]+' m='+this.mode+' es='+(epx === sx && epy === sy)+' t='+Date.now(), c: '#ffffff'});
     for (const i in coords) {
       const x = coords[i][0] + epx, y = coords[i][1] + epy;
       if (x > 0 && y > 0 && x < 30 && y < 30) coords[i] = { x, y, d: Math.sqrt((x-tpx)**2+(y-tpy)**2) };
