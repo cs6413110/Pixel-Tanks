fca62da3fc1dddf798c37a590080dd923fb1889a
cs6413110 <129222222+cs6413110@users.noreply.github.com>
2023-07-28 12:27:18 -0700

    fix firing angles

--- public/js/pixel-tanks.js
+++ public/js/pixel-tanks.js
@@ -1611,8 +1611,14 @@ function Game() {
     }
 
     toPoint(angle) {
-      const theta = (-angle)*Math.PI/180, y = Math.cos(theta), x = Math.sin(theta);
-      return {x: x/Math.abs(x), y: y/Math.abs(x)};
+      const theta = (-angle) * Math.PI / 180;
+      const y = Math.cos(theta);
+      const x = Math.sin(theta);
+      if (x === 0) {
+        return { x: 0, y: y / Math.abs(y) };
+      } else {
+        return { x: x / Math.abs(x), y: y / Math.abs(x) };
+      }
     }
 
     mousedown(e) {
