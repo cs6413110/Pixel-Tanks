c2c0d5ea9ec3227051cdace78d623f08c218e35c
cs6413110 <129222222+cs6413110@users.noreply.github.com>
2023-08-04 09:58:51 -0700

    Update pixel-tanks.js

--- public/js/pixel-tanks.js
+++ public/js/pixel-tanks.js
@@ -1535,9 +1535,9 @@ function Game() {
         GUI.draw.fillStyle = PixelTanks.userData.color;
         GUI.drawImage(PixelTanks.images.items[PixelTanks.userData.items[i]], c[i], 900, 100, 100, 1);
         GUI.draw.fillRect(c[i], 900+Math.min((Date.now()-this.timers.items[i].time)/this.timers.items[i].cooldown, 1)*100, 100, 100);
-        GUI.draw.globalAlpha = .2;
+        GUI.draw.globalAlpha = .5;
         GUI.draw.fillStyle = '#000000';
-        if (this['canItem'+i]) GUI.draw.fillRect(c[i], 900, 100, 100);
+        if (!this['canItem'+i]) GUI.draw.fillRect(c[i], 900, 100, 100);
         GUI.draw.globalAlpha = 1;
       }
       for (let i = 0; i < 3; i++) {
