f8803ca9377ddfde28598d242ccbbeb8102c66a6
cs6413110 <129222222+cs6413110@users.noreply.github.com>
2023-08-08 13:04:54 -0700

    Update engine.js

--- public/js/engine.js
+++ public/js/engine.js
@@ -680,7 +680,7 @@ class AI {
       if (!limiter.includes(p.length)) {
         coords.splice(r, 1);
         i++;
-        if (i >= 5 && mode !== 0) return this.path = {p: finder.findPath(sx, sy, mode === 1 ? tx : sx+(tx-sx), mode === 1 ? ty : sy+(ty-sy), map.clone()).slice(0, 5), m: this.mode, t: Date.now(), o: Date.now()};
+        if (i >= 5 && mode !== 0) return this.path = {p: finder.findPath(sx, sy, mode === 1 ? tx : sx+(tx-sx), mode === 1 ? ty : sy+(ty-sy), this.host.map.clone()).slice(0, 5), m: this.mode, t: Date.now(), o: Date.now()};
         if (coords.length === 0) return this.path = { p: [], m: this.mode, t: Date.now(), o: Date.now()};
       } else {
         this.path = { p, m: this.mode, t: Date.now(), o: Date.now()};
