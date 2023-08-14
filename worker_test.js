75349c0116a434fcef2e3aa2782da2e6a3c648c1
cs6413110 <129222222+cs6413110@users.noreply.github.com>
2023-08-14 10:40:11 -0700

    Update worker_test.js

--- worker_test.js
+++ worker_test.js
@@ -39,7 +39,7 @@ setInterval(async () => {
   console.log('Assigning workers+1 tasks');
   let start = Date.now();
   for (let i = 0; i <= Compute.workers.length; i++) {
-    Compute.pushWork('collider', 0, 0, 1600, 1000, blocks).then((r) => console.log('Worker #'+i+' finished => '+r));
+    Compute.pushWork('collider', 0, 0, 1600, 1000, blocks).then((r) => console.log('Worker #'+i+' finished'));
   }
   
   let end = Date.now()-start;
