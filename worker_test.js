74bf98221ed19ceac46ca32a8099380c56821b7b
cs6413110 <129222222+cs6413110@users.noreply.github.com>
2023-08-14 16:14:57 -0700

    Update worker_test.js

--- worker_test.js
+++ worker_test.js
@@ -37,13 +37,7 @@ Compute.initialize(4);
 
 const blocks = [];
 for (let i = 0; i < 1000; i++) blocks.push(Math.random()*2000-200, Math.random()*1400-200);
-new Worker('process.env.SET_IN_WORKER = "foo"', { eval: true, env: SHARE_ENV })
-  .on('exit', () => {
-    console.log(process.env.SET_IN_WORKER);  // Prints 'foo'.
-  }); 
-new Worker('process.env.DATA = "'+JSON.stringify([0, 0, 1600, 1000, blocks])+'"', { eval: true, env: SHARE_ENV }).on('exit', () => {
-  console.log(process.env.DATA);
-}); 
+new Worker('process.env.DATA = "'+JSON.stringify([0, 0, 1600, 1000, blocks])+'"', { eval: true, env: SHARE_ENV }); 
 
 setInterval(async () => {
   let counter = 0, startThreaded = Date.now();
