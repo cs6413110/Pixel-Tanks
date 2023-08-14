1a850d30a12bc2a2bad788ce76bb781edb5f23b1
cs6413110 <129222222+cs6413110@users.noreply.github.com>
2023-08-14 13:34:08 -0700

    Update worker_test.js

--- worker_test.js
+++ worker_test.js
@@ -1,5 +1,4 @@
-const Threads = require('threads');
-const {spawn, Worker} = Threads;
+const { Worker } = require('worker_threads');
 
 const collision = (x, y, w, h, x2, y2, w2, h2) => (x + w > x2 && x < x2 + w2 && y + h > y2 && y < y2 + h2);
 const collider = (x, y, w, h, rects) => {
@@ -7,6 +6,7 @@ const collider = (x, y, w, h, rects) => {
   for (let i = 0; i < rects.length; i++) if (collision(x, y, w, h, rects[i][0], rects[i][1], rects[i][2], rects[i][3])) collided.push(i);
   return collided;
 }
+
 class Compute {
   static initialize(t) {
     this.workers = [];
@@ -14,7 +14,7 @@ class Compute {
   }
 
   static async pushWorker() {
-    const worker = await spawn(new Worker('./public/js/compute.js'));
+    const worker = new Worker('./public/js/compute.js');
     worker.ready = true;
     this.workers.push(worker);
     return worker;
@@ -24,12 +24,9 @@ class Compute {
     let worker = this.workers.find(w => w.ready);
     if (!worker) worker = await this.pushWorker();
     worker.ready = false;
-    let start = Date.now();
-    worker[id](...params).then(o => {
-      worker.ready = true;
-      console.log('worker took '+(Date.now()-start));
-      callback(o);
-    });
+    worker.postMessage({task: id, params});
+    worker.on('message', callback);
+    worker.on('exit', console.log);
   }
 }
 Compute.initialize(4);
