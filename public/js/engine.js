3aab3943cf7291f7bdb24d6ba7f734c121ace16f
cs6413110 <129222222+cs6413110@users.noreply.github.com>
2023-08-14 08:16:35 -0700

    Update engine.js

--- public/js/engine.js
+++ public/js/engine.js
@@ -3,7 +3,7 @@ try {
   Threads = require('threads');
   PF = require('pathfinding');
 } catch (e) {}
-
+if (!Threads) Threads = {spawn: () => {}, Thread: 0, Worker: 0};
 const {spawn, Thread, Worker} = Threads;
 
 const collision = (x, y, w, h, x2, y2, w2, h2) => (x + w > x2 && x < x2 + w2 && y + h > y2 && y < y2 + h2);
