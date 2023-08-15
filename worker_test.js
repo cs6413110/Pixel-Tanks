f59c8199ee69dd247039c9cd1bf9f46e0e336ded
cs6413110 <129222222+cs6413110@users.noreply.github.com>
2023-08-14 19:08:31 -0700

    Update worker_test.js

--- worker_test.js
+++ worker_test.js
@@ -1,10 +1,24 @@
 const { Worker, SHARE_ENV } = require('worker_threads');
 
-const collision = (x, y, w, h, x2, y2, w2, h2) => (x + w > x2 && x < x2 + w2 && y + h > y2 && y < y2 + h2);
-const collider = (x, y, w, h, rects) => {
-  const collided = [];
-  for (let i = 0; i < rects.length; i++) if (collision(x, y, w, h, rects[i][0], rects[i][1], rects[i][2], rects[i][3])) collided.push(i);
-  return collided;
+const raycast = (x, y, x2, y2, w) => {
+  const dx = x-x2, dy = y-y2, adx = Math.abs(dx), ady = Math.abs(dy);
+  const minx = Math.min(x, x2), miny = Math.min(y, y2), maxx = Math.max(x, x2), maxy = Math.max(y, y2);
+  const walls = w.filter(w => collision(w.x, w.y, 100, 100, minx, miny, adx, ady));
+  let px = Array.from({length: adx+1}, (_, i) => minx+i), py = Array.from({length: ady+1}, (_, i) => miny+i);
+  for (const w of walls) {
+    if (w.x%100 !== 0) px.push(w.x, w.x+100);
+    if (w.y%100 !== 0) py.push(w.y, w.y+100);
+  }
+  if (dx === 0) {
+    for (const p of py) for (const w of walls) if (collision(w.x, w.y, 100, 100, x-.5, p-.5, 1, 1)) return false;
+  } else {
+    const o = y-(dy/dx)*x;
+    for (const w of walls) {
+      for (const p of py) if (collision(w.x, w.y, 100, 100, (p-o)/(dy/dx)-.5, p-.5, 1, 1)) return false;
+      for (const p of px) if (collision(w.x, w.y, 100, 100, p-.5, (dy/dx)*p+o-.5, 1, 1)) return false;
+    }
+  }
+  return true;
 }
 
 class Compute {
@@ -36,12 +50,12 @@ class Compute {
 Compute.initialize(4);
 
 const blocks = [];
-for (let i = 0; i < 100; i++) blocks.push([Math.random()*2000-200, Math.random()*1400-200]);
+for (let i = 0; i < 100; i++) blocks.push({x: Math.random()*2000-200, y: Math.random()*1400-200});
 
 setInterval(async () => {
   let counter = 0, startThreaded = Date.now();
   for (let i = 0; i < Compute.workers.length; i++) {
-    Compute.pushWork('collider', r => {
+    Compute.pushWork('raycast', r => {
       counter++;
       if (counter === Compute.workers.length) console.log('Threaded took '+(Date.now()-startThreaded)+'ms');
     }, 0, 0, 1600, 1000, blocks);
@@ -51,7 +65,7 @@ setInterval(async () => {
 setTimeout(() => {
   setInterval(() => {
     let startSync = Date.now();
-    for (let i = 0; i < Compute.workers.length; i++) collider(0, 0, 1600, 1000, blocks);
+    for (let i = 0; i < Compute.workers.length; i++) raycast(0, 0, 1600, 1000, blocks);
     console.log('Sync took '+(Date.now()-startSync)+'ms');
   }, 10000);
 }, 5000);
