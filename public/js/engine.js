e837a954b787fb86333f09899f927deb1f77602d
cs6413110 <129222222+cs6413110@users.noreply.github.com>
2023-08-15 08:25:17 -0700

    Update engine.js

--- public/js/engine.js
+++ public/js/engine.js
@@ -128,7 +128,7 @@ class Engine {
       const coordinates = [{ r: [337.5, 360], dx: -10, dy: 80 }, { r: [0, 22.5], dx: -10, dy: 80 }, { r: [22.5, 67.5], dx: -100, dy: 80 }, { r: [67.5, 112.5], dx: -100, dy: -10 }, { r: [112.5, 157.5], dx: -100, dy: -100 }, { r: [157.5, 202.5], dx: -10, dy: -100 }, { r: [202.5, 247.5], dx: 80, dy: -100 }, { r: [247.5, 292.5], dx: 80, dy: -10 }, { r: [292.5, 337.5], dx: 80, dy: 80 }];
       for (const coord of coordinates) {
         if (r >= coord.r[0] && r < coord.r[1]) {
-          this.b.push(new Block(t.x + coord.dx, t.y + coord.dy, { strong: 200, weak: 100, gold: 300, mine: 0, spike: 0, fortress: 150 }[data.blockType], data.blockType, t.team, this));
+          this.b.push(new Block(t.x + coord.dx, t.y + coord.dy, { strong: 200, weak: 100, gold: 300, mine: 0, spike: 0, fortress: 300 }[data.blockType], data.blockType, t.team, this));
           break;
         }
       }
