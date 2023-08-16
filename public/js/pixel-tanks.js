e7857920ef70e480640895f84018e6f493c4c150
cs6413110 <129222222+cs6413110@users.noreply.github.com>
2023-08-16 13:37:07 -0700

    Update pixel-tanks.js

--- public/js/pixel-tanks.js
+++ public/js/pixel-tanks.js
@@ -365,6 +365,8 @@ function Game() {
       Menus.scaler = document.createElement('CANVAS');
       GUI.canvas = document.createElement('CANVAS');
       GUI.draw = GUI.canvas.getContext('2d');
+      GUI.draw.imageSmoothingEnabled = false;
+      Menus.scaler.getContext('2d').imageSmoothingEnabled = false;
       document.body.appendChild(GUI.canvas);
       PixelTanks.resizer = window.innerHeight/1000;
       GUI.canvas.height = window.innerHeight;
