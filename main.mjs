40c6f1d6c3d7777aedf9526df8a16cfab49a1e1f
cs6413110 <129222222+cs6413110@users.noreply.github.com>
2023-08-17 06:19:09 -0700

    Update main.mjs

--- main.mjs
+++ main.mjs
@@ -1,14 +1,15 @@
-import {Server, Router} from 'hyper-express';
+import express from 'express';
+import expressWs from 'express-ws';
 import {promises as fs} from 'fs';
 import {MongoClient} from 'mongodb';
 import msgpack from 'msgpack-lite';
 import Filter from 'bad-words';
 import TokenGenerator from 'uuid-token-generator';
-import {Core} from './ffa-server.mjs';
+import {ffa} from './ffa-server.mjs';
 
 const connectionString = 'mongodb+srv://cs641311:355608-G38@cluster0.z6wsn.mongodb.net/?retryWrites=true&w=majority', port = 80;
 
-const HyperExpressServer = new Server({fast_buffers: true}), router = new Router(), client = new MongoClient(connectionString), filter = new Filter(), tokgen = new TokenGenerator(256, TokenGenerator.BASE62), tokens = new Set(), sockets = [];
+const app = express(), client = new MongoClient(connectionString), filter = new Filter(), tokgen = new TokenGenerator(256, TokenGenerator.BASE62), tokens = new Set(), sockets = [];
 let db;
 
 (async () => {
@@ -47,7 +48,8 @@ const routes = {
   },
 };
 
-router.ws('/', {idle_timeout: Infinity}, (socket) => {
+expressWs(app);
+app.ws('/', socket => {
   sockets.push(socket);
   socket._send = socket.send;
   socket.send = (data) => socket._send(msgpack.encode(data));
@@ -68,10 +70,7 @@ router.ws('/', {idle_timeout: Infinity}, (socket) => {
   });
 });
 
-HyperExpressServer.get('/verify', (req, res) => res.end(valid(req.query.token, req.query.username).toString()));
-HyperExpressServer.get('/*', async(req, res) => {
-  res.header('Content-Type', 'application/javascript').end(await fs.readFile('./public/js/pixel-tanks.js'));
-});
-HyperExpressServer.use(router);
-HyperExpressServer.use(Core);
+app.get('/verify', (req, res) => res.end(valid(req.query.token, req.query.username).toString()));
+app.get('/*', async(req, res) => res.header('Content-Type', 'application/javascript').end(await fs.readFile('./public/js/pixel-tanks.js')));
+app.use(ffa);
 HyperExpressServer.listen(port);
