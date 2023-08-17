06d1baeda678156c4bb7b4030b4a7c69b5de6c52
cs6413110 <129222222+cs6413110@users.noreply.github.com>
2023-08-17 07:34:47 -0700

    Update main.mjs

--- main.mjs
+++ main.mjs
@@ -68,12 +68,7 @@ app.ws('/', socket => {
   });
 });
 
-app.get('/verify', (req, res) => {
-  console.log('Attempting to verify');
-  res.end(valid(req.query.token, req.query.username).toString());
-});
-app.get('/*', async(req, res) => {
-  res.header('Content-Type', 'application/javascript').end(await fs.readFile('./public/js/pixel-tanks.js'))
-});
 app.use(ffa);
+app.get('/verify', (req, res) => res.end(valid(req.query.token, req.query.username).toString()));
+app.get('/*', async(req, res) => res.header('Content-Type', 'application/javascript').end(await fs.readFile('./public/js/pixel-tanks.js')));
 app.listen(port);
