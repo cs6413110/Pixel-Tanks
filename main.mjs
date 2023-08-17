f0186930b862ceabacf7b34f53e69777e2882f3e
cs6413110 <129222222+cs6413110@users.noreply.github.com>
2023-08-16 17:40:55 -0700

    Update main.mjs

--- main.mjs
+++ main.mjs
@@ -52,8 +52,10 @@ router.ws('/', {idle_timeout: Infinity}, (socket) => {
   socket._send = socket.send;
   socket.send = (data) => socket._send(msgpack.encode(data));
   socket.on('message', (data) => {
-    try {
+    try 
+      console.log(data);
       data = msgpack.decode(data);
+      console.log(data);
     } catch (e) {
       return socket.destroy();
     }
