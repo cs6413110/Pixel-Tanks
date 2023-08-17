a440d08b2ae547b3a5e44269c6b2fc3ea4c5b121
cs6413110 <129222222+cs6413110@users.noreply.github.com>
2023-08-17 07:13:32 -0700

    Update pixel-tanks.js

--- public/js/pixel-tanks.js
+++ public/js/pixel-tanks.js
@@ -41,7 +41,7 @@ function Game() {
     connect() {
       this.socket = new WebSocket(this.url);
       this.socket.onopen = () => {
-        this.socket.binaryType = 'arraybuffer';
+        //this.socket.binaryType = 'arraybuffer';
         this.status = 'connected';
         if (this.options.keepAlive) this.socket.keepAlive = setInterval(() => {
           this.socket.send('|');
