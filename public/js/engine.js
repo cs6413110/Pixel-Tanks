90103466ca30468ea6685919f5cf918ee8758519
cs641311 <42550108+starlite-developer@users.noreply.github.com>
2023-08-21 13:43:44 -0700

    Update engine.js

--- public/js/engine.js
+++ public/js/engine.js
@@ -72,7 +72,7 @@ class Engine {
     }
     t.r = r;
     if (t.ded) return;
-    if (t.immune && (t.class === 'fire' || t.class === 'builderd')) {
+    if (t.immune && (t.class === 'fire' || t.class === 'builder')) {
       const team = this.parseTeamExtras(t.team), type = t.class === 'fire' ? 'fire' : 'weak';
       if ((t.x + 80) % 100 > 80 && [45, 90, 135].includes(t.baseRotation)) this.b.push(new Block(Math.floor(t.x / 100) * 100 + 100, Math.floor(t.y / 100) * 100, 100, type, team, this));
       if (t.x % 100 < 20 && [225, 270, 315].includes(t.baseRotation)) this.b.push(new Block(Math.floor(t.x / 100) * 100 - 100, Math.floor(t.y / 100) * 100, 100, type, team, this));
@@ -146,8 +146,8 @@ class Engine {
       }
     }
     if (use.includes('turret')) {
-      //this.ai = this.ai.filter(ai => this.getUsername(ai.team) !== t.username);
-      this.ai.push(new AI(Math.floor(t.x / 100) * 100 + 10, Math.floor(t.y / 100) * 100 + 10, Math.floor(Math.random()*3), t.rank, t.team, this));
+      this.ai = this.ai.filter(ai => this.getUsername(ai.team) !== t.username);
+      this.ai.push(new AI(Math.floor(t.x / 100) * 100 + 10, Math.floor(t.y / 100) * 100 + 10, 0, t.rank, t.team, this));
     }
     if (use.includes('buff')) {
       t.buff = true;
