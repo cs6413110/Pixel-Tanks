18462ed07d4184498448b060950cf9db5e67d75e
cs641311 <42550108+starlite-developer@users.noreply.github.com>
2023-08-22 15:16:07 -0700

    Update ffa-server.mjs

--- ffa-server.mjs
+++ ffa-server.mjs
@@ -416,14 +416,28 @@ class Multiplayer extends Engine {
   }
 
   send() {
-    const view = {x: -860, y: -560, w: 1880, h: 1280};
     for (const t of this.pt) {
-      const message = {blocks: [], tanks: [], ai: [], bullets: [], explosions: [], logs: this.logs, tickspeed, event: 'hostupdate'};
-      for (const b of this.b) if (A.collider(b.x, b.y, 100, 100, t.x+view.x, t.y+view.y, view.w, view.h)) message.blocks.push(b.raw);
-      for (const pt of this.pt) if (A.collider(pt.x, pt.y, 80, 80, t.x+view.x, t.y+view.y, view.w, view.h)) message.tanks.push(pt.raw);
-      for (const ai of this.ai) if (A.collider(ai.x, ai.y, 80, 80, t.x+view.x, t.y+view.y, view.w, view.h)) message.ai.push(ai.raw);
-      for (const s of this.s) if (A.collider(s.x, s.y, 10, 10, t.x+view.x, t.y+view.y, view.w, view.h)) message.bullets.push(s.raw);
-      for (const d of this.d) if (A.collider(d.x, d.y, d.w, d.h, t.x+view.x, t.y+view.y, view.w, view.h)) message.explosions.push(d.raw);
+      const time = Date.now()-15, view = {x: t.x-860, y: t.y-560, w: 1880, h: 1280}, message = {b: [], pt: [], ai: [], s: [], d: [], logs: this.logs, tickspeed, event: 'hostupdate'};
+      for (const b of this.b) {
+        if (b.updatedLast < time) continue;
+        message.b.push(b.raw);
+      }
+      for (const pt of this.pt) {
+        if (pt.updatedLast < time) continue;
+        message.pt.push(pt.raw);
+      }
+      for (const ai of this.ai) {
+        if (ai.updatedLast < time) continue;
+        message.ai.push(ai.raw);
+      }
+      for (const s of this.s) {
+        if (s.updatedLast < time) continue;
+        message.s.push(s.raw);
+      }
+      for (const d of this.d) {
+        if (d.updatedLast < time) continue;
+        message.d.push(d.raw);
+      }
       t.socket.send(message);
       outgoing_per_second++;
     }
