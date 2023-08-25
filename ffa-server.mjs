38a6c6d9ca760f43e32ab1ca522c47f0c033811d
cs641311 <42550108+starlite-developer@users.noreply.github.com>
2023-08-25 08:40:49 -0700

    Update ffa-server.mjs

--- ffa-server.mjs
+++ ffa-server.mjs
@@ -523,8 +523,8 @@ class FFA extends Multiplayer {
 }
 
 class DUELS extends Engine {
-  constructor(ip) {
-    super(ip, duelsLevels);
+  constructor(duelsLevels) {
+    super(ip, );
     this.mode = 0; // 0 -> waiting for players to join, 1 -> waiting for second player to join, 2 -> 10 second ready timer, 3 -> match active, 4 -> gameover and server shutdown
   }
 
