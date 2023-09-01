d19540b0cc814579bc059c25efdacf0ab663132f
cs641311 <42550108+starlite-developer@users.noreply.github.com>
2023-09-01 13:23:13 -0700

    add class to data for medic thingy circle idk

--- public/js/engine.js
+++ public/js/engine.js
@@ -250,7 +250,7 @@ class Tank {
   constructor(data, host) {
     this.raw = {};
     this.render = {b: new Set(), s: new Set(), pt: new Set(), d: new Set(), ai: new Set()};
-    ['rank', 'username', 'cosmetic', 'color', 'damage', 'maxHp', 'hp', 'shields', 'team', 'x', 'y', 'r', 'ded', 'pushback', 'baseRotation', 'baseFrame', 'fire', 'damage', 'animation', 'buff', 'invis', 'id'].forEach(p => {
+    ['rank', 'username', 'cosmetic', 'color', 'damage', 'maxHp', 'hp', 'shields', 'team', 'x', 'y', 'r', 'ded', 'pushback', 'baseRotation', 'baseFrame', 'fire', 'damage', 'animation', 'buff', 'invis', 'id', 'class'].forEach(p => {
       Object.defineProperty(this, p, {
         get() {
           return this.raw[p];
