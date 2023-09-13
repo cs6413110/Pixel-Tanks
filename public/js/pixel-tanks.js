faba65ee365d99e194a4eaa4b09440d4efa84c96
cs6413110 <129222222+cs6413110@users.noreply.github.com>
2023-09-13 10:20:50 -0700

    Update pixel-tanks.js

--- public/js/pixel-tanks.js
+++ public/js/pixel-tanks.js
@@ -1489,7 +1489,7 @@ function Game() {
       PixelTanks.renderTop(t.x, t.y, 80, t.color, t.r, t.pushback);
       GUI.drawImage(PixelTanks.images.tanks.top, t.x, t.y, 80, 90, a, 40, 40, 0, t.pushback, t.r);
       if (t.cosmetic) GUI.drawImage(PixelTanks.images.cosmetics[t.cosmetic], t.x, t.y, 80, 90, a, 40, 40, 0, t.pushback, t.r);
-      if ((!t.ded && getTeam(this.team) === getTeam(t.team)) || (this.ded && !t.ded) || (PixelTanks.userData.class === 'tactical' && !t.ded)) {
+      if ((!t.ded && getTeam(this.team) === getTeam(t.team)) || (this.ded && !t.ded) || (PixelTanks.userData.class === 'tactical' && !t.ded && Math.sqrt(Math.pow(t.x-this.tank.x, 2)+Math.pow(t.y-this.tank.y, 2)) < 200)) {
         GUI.draw.fillStyle = '#000000';
         GUI.draw.fillRect(t.x-2, t.y+98, 84, 11);
         GUI.draw.fillStyle = '#00FF00';
