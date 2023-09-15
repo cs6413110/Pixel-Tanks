1e4a4363c60a7d8d2f6f66ed38aee88f40f2b15f
cs6413110 <129222222+cs6413110@users.noreply.github.com>
2023-09-15 08:40:07 -0700

    Update pixel-tanks.js

--- public/js/pixel-tanks.js
+++ public/js/pixel-tanks.js
@@ -1286,6 +1286,7 @@ function Game() {
       this.msg = '';
       this.multiplayer = multiplayer;
       this.tank = {use: [], fire: [], r: 0, x: 0, y: 0};
+      this.tank.invis = PixelTanks.userData.class === 'stealth';
       this.ops = 0;
       this.ups = 0;
       this.fps = 0;
