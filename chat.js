ba7b2d90cc7fb1096629cb0e650aa2cca8305be4
cs6413110 <129222222+cs6413110@users.noreply.github.com>
2023-09-21 07:41:31 -0700

    megasocket version bump

--- chat.js
+++ chat.js
@@ -13,68 +13,76 @@ document.body.appendChild(message);
 document.body.appendChild(server);
 document.body.appendChild(join);
 class MegaSocket {
-  constructor(url, options={keepAlive: true, autoconnect: true, reconnect: false}) {
-    this.url = url;
-    this.options = options;
-    this.callstack = {open: [], close: [], message: []};
-    if (this.options.autoconnect) {
-      this.status = 'connecting';
-      this.connect();
-    } else {
+    constructor(url, options={keepAlive: true, autoconnect: true, reconnect: false}) {
+      this.url = url;
+      this.options = options;
+      this.callstack = {open: [], close: [], message: []};
       this.status = 'idle';
       window.addEventListener('offline', () => {
         this.socket.close();
         this.socket.onclose();
       });
       if (this.options.reconnect) window.addEventListener('online', this.connect.bind(this));
-    }
-  }
-
-  connect() {
-    this.socket = new WebSocket(this.url);
-    this.socket.onopen = () => {
-      this.socket.binaryType = 'arraybuffer';
-      this.status = 'connected';
-      if (this.options.keepAlive) this.socket.keepAlive = setInterval(() => {
-        this.socket.send(msgpack.encode({op: 'ping'}));
-      }, 50000);
-      this.callstack.open.forEach(f => f());
-    }
-    this.socket.onmessage = data => {
-      try {
-        data = msgpack.decode(new Uint8Array(data.data));
-      } catch(e) {
-        alert('Socket Encryption Error: ' + data.data+' | '+e);
+      if (this.options.autoconnect) {
+        this.status = 'connecting';
+        this.connect();
       }
-      if (data.status === 'error') return alert(data.message);
-      this.callstack.message.forEach(f => f(data));
     }
-    this.socket.onclose = e => {
-      clearInterval(this.socket.keepAlive);
-      this.status = 'disconnected';
-      this.callstack.close.forEach(f => f());
-      if (this.options.reconnect) this.connect();
-    }
-  }
 
-  on(event, operation) {
-    if (event === 'connect') this.callstack.open.push(operation);
-    if (event === 'message') this.callstack.message.push(operation);
-    if (event === 'close') this.callstack.close.push(operation);
+    connect() {
+      this.socket = new WebSocket(this.url);
+      this.socket.onopen = () => {
+        this.socket.binaryType = 'arraybuffer';
+        this.status = 'connected';
+        if (this.options.keepAlive) this.socket.keepAlive = setInterval(() => {
+          this.socket.send(msgpack.encode({type: 'ping', op: 'ping'}));
+        }, 30000);
+        this.callstack.open.forEach(f => f());
+      }
+      this.socket.onmessage = data => {
+        try {
+          data = msgpack.decode(new Uint8Array(data.data));
+        } catch(e) {
+          alert('Socket Encryption Error: ' + data.data+' | '+e);
+        }
+        if (data.status === 'error') {
+          if (data.message === 'Invalid token.') {
+            clearInterval(PixelTanks.autosave);
+            if (PixelTanks.user.player) PixelTanks.user.player.implode();
+            PixelTanks.user.token = undefined;
+            PixelTanks.user.username = undefined;
+            return Menus.trigger('start');
+          }
+          return alert(data.message);
+        }
+        this.callstack.message.forEach(f => f(data));
+      }
+      this.socket.onclose = e => {
+        clearInterval(this.socket.keepAlive);
+        this.status = 'disconnected';
+        this.callstack.close.forEach(f => f());
+        if (this.options.reconnect) this.connect();
+      }
+    }
+
+    on(event, operation) {
+      if (event === 'connect') this.callstack.open.push(operation);
+      if (event === 'message') this.callstack.message.push(operation);
+      if (event === 'close') this.callstack.close.push(operation);
+    }
+    no(event) {
+      if (event === 'connect') this.callstack.open = [];
+      if (event === 'message') this.callstack.message = [];
+      if (event === 'close') this.callstack.close = [];
+    }
+    send(data) {
+      data = msgpack.encode(data);
+      this.socket.send(data);
+    }
+    close() {
+      this.socket.close();
+    }
   }
-  no(event) {
-    if (event === 'connect') this.callstack.open = [];
-    if (event === 'message') this.callstack.message = [];
-    if (event === 'close') this.callstack.close = [];
-  }
-  send(data) {
-    data = msgpack.encode(data);
-    this.socket.send(data);
-  }
-  close() {
-    this.socket.close();
-  }
-}
 
 let room = '';
 const username = 'screamFix';
