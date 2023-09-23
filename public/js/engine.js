2bd492d51f0a801791a92f71235b151f7a50de02
cs6413110 <129222222+cs6413110@users.noreply.github.com>
2023-09-23 11:02:20 -0700

    Update engine.js

--- public/js/engine.js
+++ public/js/engine.js
@@ -801,7 +801,7 @@ class AI {
       this.host.cells[cx][cy].add(this);
       cells.push({x: cx, y: cy});
     }
-    for (const cell of this.cells.filter(c => !cells.includes(c))) this.cells[cell.x][cell.y].delete(this);
+    for (const cell of this.cells.filter(c => !cells.includes(c))) this.host.cells[cell.x][cell.y].delete(this);
     this.cells = cells;
   }
 
