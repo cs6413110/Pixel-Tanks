bde462ec2376f870a051f4b102c1c6eaf1d1605c
cs6413110 <129222222+cs6413110@users.noreply.github.com>
2023-10-06 13:51:06 -0700

    Update lag.js

--- lag.js
+++ lag.js
@@ -37,29 +37,3 @@ setTimeout(() => {
     console.log('Maximum: '+max);
   }, 60000);
 }, 5000);
-client.onerror = function() {
-    console.log('Connection Error');
-};
-
-client.onopen = function() {
-    console.log('WebSocket Client Connected');
-
-    function sendNumber() {
-        if (client.readyState === client.OPEN) {
-            var number = Math.round(Math.random() * 0xFFFFFF);
-            client.send(number.toString());
-            setTimeout(sendNumber, 1000);
-        }
-    }
-    sendNumber();
-};
-
-client.onclose = function() {
-    console.log('echo-protocol Client Closed');
-};
-
-client.onmessage = function(e) {
-    if (typeof e.data === 'string') {
-        console.log("Received: '" + e.data + "'");
-    }
-};
