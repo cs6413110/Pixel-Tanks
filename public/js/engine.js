9605eb662af2193389d412de66e02a099051310a
cs6413110 <129222222+cs6413110@users.noreply.github.com>
2023-10-16 10:32:31 -0700

    fix?

--- public/js/engine.js
+++ public/js/engine.js
@@ -453,6 +453,12 @@ class Shot {
     this.y = data.y-5;
     this.sx = this.x;
     this.sy = this.y;
+    this.cells = new Set();
+    for (let dx = this.x/100, dy = this.y/100, i = 0; i < 4; i++) {
+      const cx = Math.max(0, Math.min(29, Math.floor(i < 2 ? dx : dx+.09))), cy = Math.max(0, Math.min(29, Math.floor(i % 2 ? dy : dy+.09)));
+      host.cells[cx][cy].add(this);
+      this.cells.add(cx+'x'+cy);
+    }
     this.u();
   }
 
@@ -604,6 +610,10 @@ class Shot {
   destroy() {
     const index = this.host.s.indexOf(this);
     if (index !== -1) this.host.s.splice(index, 1);
+    for (const cell of this.cells) {
+      const [x, y] = cell.split('x');
+      this.host.cells[x][y].delete(this);
+    }
   }
 }
 
@@ -619,6 +629,12 @@ class Damage {
     this.raw = {};
     this.id = Math.random();
     this.f = 0;
+    this.cells = new Set();
+    for (let dx = this.x/100, dy = this.y/100, i = 0; i < 4; i++) {
+      const cx = Math.max(0, Math.min(29, Math.floor(i < 2 ? dx : dx+w-.01))), cy = Math.max(0, Math.min(29, Math.floor(i % 2 ? dy : dy+w-.01)));
+      host.cells[cx][cy].add(this);
+      this.cells.add(cx+'x'+cy);
+    }
     const cells = new Set(), cache = new Set();
     for (let dx = this.x/100, dy = this.y/100, i = 0; i < 4; i++) cells.add(Math.max(0, Math.min(29, Math.floor(i < 2 ? dx : dx + this.w/100)))+'x'+Math.max(0, Math.min(29, Math.floor(i % 2 ? dy : dy + this.h/100))));
     for (const cell of cells) {
@@ -655,6 +671,10 @@ class Damage {
     clearInterval(this.i);
     const index = this.host.d.indexOf(this);
     if (index !== -1) this.host.d.splice(index, 1);
+    for (const cell of this.cells) {
+      const [x, y] = cell.split('x');
+      this.host.cells[x][y].delete(this);
+    }
   }
 }
 
@@ -727,7 +747,7 @@ class AI {
     if (this.role !== 0) this.move();
     const cells = new Set();
     for (let dx = this.x/100, dy = this.y/100, i = 0; i < 4; i++) {
-      const cx = Math.max(0, Math.min(29, Math.floor(i < 2 ? dx : dx + (this.role === 0 ? 1 : .8)))), cy = Math.max(0, Math.min(29, Math.floor(i % 2 ? dy : dy + (this.role === 0 ? 1 : .8))));
+      const cx = Math.max(0, Math.min(29, Math.floor(i < 2 ? dx : dx + (this.role === 0 ? .99 : .79)))), cy = Math.max(0, Math.min(29, Math.floor(i % 2 ? dy : dy + (this.role === 0 ? .99 : .79))));
       this.host.cells[cx][cy].add(this);
       cells.add(cx+'x'+cy);
     }
