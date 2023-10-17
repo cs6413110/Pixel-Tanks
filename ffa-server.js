78fdbe69941576514e2529d44c887942c029d12e
cs6413110 <129222222+cs6413110@users.noreply.github.com>
2023-10-17 08:56:53 -0700

    Update ffa-server.js

--- ffa-server.js
+++ ffa-server.js
@@ -439,8 +439,6 @@ class A {
 class Multiplayer extends Engine {
   constructor(levels) {
     super(levels);
-    this.message = {b: [], pt: [], ai: [], s: [], d: [], logs: this.logs, global: this.global, tickspeed, event: 'hostupdate', delete: {b: [], pt: [], ai: [], s: [], d: []}};
-    this.newrender = {b: new Set(), pt: new Set(), ai: new Set(), s: new Set(), d: new Set(), logs: this.logs.length};
     if (!SETTINGS.fps_boost) this.i.push(setInterval(() => this.send(), 1000/SETTINGS.UPS));
   }
 
@@ -450,6 +448,8 @@ class Multiplayer extends Engine {
 
   add(socket, data) {
     data.socket = socket;
+    data.message = {b: [], pt: [], ai: [], s: [], d: [], logs: this.logs, global: this.global, tickspeed, event: 'hostupdate', delete: {b: [], pt: [], ai: [], s: [], d: []}};
+    data.newrender = {b: new Set(), pt: new Set(), ai: new Set(), s: new Set(), d: new Set(), logs: this.logs.length};
     this.logs.push({m: this.joinMsg(data.username), c: '#66FF00'});
     super.add(data);
   }
@@ -461,13 +461,16 @@ class Multiplayer extends Engine {
   send() {
     const key = {'Block': 'b', 'Shot': 's', 'AI': 'ai', 'Tank': 'pt', 'Damage': 'd'};
     for (const t of this.pt) {
-      const {x, y, lastUpdate, render, socket} = t;
-      const {newrender, message} = this;
-      console.log(newrender, message);
+      const {x, y, lastUpdate, render, socket, newrender, message} = t;
       message.tickspeed = tickspeed;
       message.global = this.global;
       message.logs = this.logs;
       newrender.logs = this.logs.length;
+      for (const entity of Object.values(key)) {
+        message[entity].length = 0;
+        message.delete[entity].length = 0;
+        newredner[entity].clear();
+      }
       let send = render.logs !== newrender.logs;
       for (let cy = 0, sy = Math.max(Math.floor(y/100)-6, 0), ey = Math.min(Math.floor(y/100)+6, 30); cy < ey; cy++) {
         for (let cx = 0, sx = Math.max(Math.floor(x/100)-9, 0), ex = Math.min(Math.floor(x/100)+9, 30); cx < ex; cx++) {
