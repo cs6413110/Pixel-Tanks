5195ce5cbc821a2ce78417d3bbdec80b40e35f0f
cs6413110 <129222222+cs6413110@users.noreply.github.com>
2023-10-19 10:19:07 -0700

    Blinkage

--- public/js/pixel-tanks.js
+++ public/js/pixel-tanks.js
@@ -1627,8 +1627,13 @@ function Game() {
       for (let i = 0; i < 4; i++) {
         GUI.drawImage(PixelTanks.images.items[PixelTanks.userData.items[i]], c[i], 900, 100, 100, 1);
         GUI.draw.fillStyle = '#000000';
-        GUI.draw.globalAlpha = .5;
-        if (!this['canItem'+i]) GUI.draw.fillRect(c[i], 900, 100, 100);
+        if (!this['canItem'+i]) {
+          GUI.draw.globalAlpha = .5;
+          GUI.draw.fillRect(c[i], 900, 100, 100);
+        } else {
+          GUI.draw.globalAlpha = Math.max(0, 100*Math.sin(Math.PI*.5*(((Date.now()-(this.timers.items[i].time+this.timers.items[i].cooldown))%4000)-3)));
+          GUI.draw.fillRect(c[i], 900, 100, 100);
+        }
         GUI.draw.globalAlpha = 1;
         GUI.draw.fillStyle = PixelTanks.userData.color;
         GUI.draw.fillRect(c[i], 900+Math.min((Date.now()-this.timers.items[i].time)/this.timers.items[i].cooldown, 1)*100, 100, 100);
