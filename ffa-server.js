f52e485e9af6bb9f9ec50a2eb98e3e66f54ef656
cs6413110 <129222222+cs6413110@users.noreply.github.com>
2023-10-20 13:56:48 -0700

    Update ffa-server.js

--- ffa-server.js
+++ ffa-server.js
@@ -7,7 +7,7 @@ const SETTINGS = {
   ppm: 400, // players per room
   log_strain: false, // Show messages per second in logs
   ups_boost: false, // Can lag clients and servers. Recommened false.
-  UPS: 30, // updates per second
+  UPS: 60, // updates per second
   filterProfanity: true, // filter profanity
   port: 15132,
   export: true,
