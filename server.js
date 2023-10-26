65ad2dfb342da858e9e54a4a4bad8f1e6b5074a2
cs6413110 <129222222+cs6413110@users.noreply.github.com>
2023-10-26 06:50:32 -0700

    Fix a ton of cringe

--- server.js
+++ server.js
@@ -1,39 +1,29 @@
 const { MongoClient } = require('mongodb');
 //const { ffa } = require('./ffa-server.js');
-
-const connectionString = 'mongodb+srv://cs641311:355608-G38@cluster0.z6wsn.mongodb.net/?retryWrites=true&w=majority';
-const client = new MongoClient(connectionString), tokens = new Set(), sockets = [];
+const client = new MongoClient'mongodb+srv://cs641311:355608-G38@cluster0.z6wsn.mongodb.net/?retryWrites=true&w=majority')
+const tokens = new Set(), sockets = [];
 const valid = (token, username) => tokens.has(`${token}:${username}`);
-const routes = {
-  auth: async ({ username, type, password }, socket) => {
-    if (!username.trim() || /\s|:/.test(username)) return socket.send({ status: 'error', message: 'Invalid username.'});
-    if (username !== filter.clean(username)) return socket.send({status: 'error', message: 'Username contains inappropriate word.'});
-    const item = await db.findOne({username}), token = tokgen.generate();
-    if (type === 'signup') {
-      if (item !== null) return socket.send({status: 'error', message: 'This account already exists.'});
-      await db.insertOne({username, password, playerdata: '{}'});
-    } else if (type === 'login') {
-      if (item === null) return socket.send({status: 'error', message: 'This account does not exist.'});
-      if (item.password !== password) return socket.send({status: 'error', message: 'Incorrect password.'});
-    } else return;
-    socket.send({status: 'success', token});
-    tokens.add(`${token}:${username}`);
-  },
-  database: async ({ token, username, type, key, value }, socket) => {
-    if (!token) return socket.send({status: 'error', message: 'No token.'});
-    if (!valid(token, username)) return socket.send({status: 'error', message: 'Invalid token.'});
-    try {
-      if (type === 'get') {
-        socket.send({status: 'success', type: 'get', data: await db.findOne({username}, {projection: {password: 0}})});
-      } else if (type === 'set') {
-        await db.findOneAndUpdate({username}, {$set: {[key]: value}});
-      } else socket.send({status: 'error', message: 'Invalid or no task.'});
-    } catch (e) {
-      socket.send({status: 'error', message: 'Error getting: '+e});
-    }
-  },
-  ping: () => {},
-};
+const auth = ({username, type, password}, socket) => {
+  const item = await db.findOne({username}), token = Math.random();
+  if (type === 'signup') {
+    if (item !== null) return socket.send({status: 'error', message: 'This account already exists.'});
+    await db.insertOne({username, password, playerdata: '{}'});
+  } else if (type === 'login') {
+    if (item === null) return socket.send({status: 'error', message: 'This account does not exist.'});
+    if (item.password !== password) return socket.send({status: 'error', message: 'Incorrect password.'});
+  } else return;
+  socket.send({status: 'success', token});
+  tokens.add(`${token}:${username}`);
+}
+const database = async({token, username, type, key, value}, socket) => {
+  if (!token) return socket.send({status: 'error', message: 'No token.'});
+  if (!valid(token, username)) return socket.send({status: 'error', message: 'Invalid Token.'});
+  if (type === 'get') {
+    socket.send({status: 'success', type, data: await db.findOne({username}, {projection: {password: 0}})});
+  } else if (type === 'set') {
+    await db.findOneAndUpdate({username}, {$set: {[key]: value}});
+  }
+}
 
 let db;
 (async () => {
@@ -63,11 +53,8 @@ const server = Bun.serve({
         return socket.close();
       }
       if (!socket.username) socket.username = data.username;
-      if (typeof routes[data.op] === 'function') {
-        routes[data.op](data, socket);
-      } else {
-        socket.send({status: 'error', message: 'Invalid or no operation.'});
-      }
+      if (data.op === 'database') database(data, socket);
+      if (data.op === 'auth') auth(data, socket);
     },
   },
 });
