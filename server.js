56a2caa14136bddf70e14e7da6781ea6c7034bba
cs6413110 <129222222+cs6413110@users.noreply.github.com>
2023-10-26 06:51:01 -0700

    Update server.jsk:NLKDNKLDGNSLKNSFLK

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const { MongoClient } = require('mongodb');
 //const { ffa } = require('./ffa-server.js');
-const client = new MongoClient'mongodb+srv://cs641311:355608-G38@cluster0.z6wsn.mongodb.net/?retryWrites=true&w=majority')
+const client = new MongoClient('mongodb+srv://cs641311:355608-G38@cluster0.z6wsn.mongodb.net/?retryWrites=true&w=majority')
 const tokens = new Set(), sockets = [];
 const valid = (token, username) => tokens.has(`${token}:${username}`);
 const auth = ({username, type, password}, socket) => {
