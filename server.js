545e08bf96bef8104c97225ea60e781b67e67dfa
cs6413110 <129222222+cs6413110@users.noreply.github.com>
2023-10-26 08:57:35 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-require('./ffa-server.js');
+//require('./ffa-server.js');
 const { MongoClient } = require('mongodb');
 const client = new MongoClient('mongodb+srv://cs641311:355608-G38@cluster0.z6wsn.mongodb.net/?retryWrites=true&w=majority')
 const tokens = new Set(), sockets = new Set();
