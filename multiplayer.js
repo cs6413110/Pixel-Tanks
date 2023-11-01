7a2406a4dda142d6f9e6baeca8c70737abd28a64
BreadSpam <143108218+BreadSpam@users.noreply.github.com>
2023-11-01 11:33:10 -0700

    Update multiplayer.js

--- multiplayer.js
+++ multiplayer.js
@@ -473,9 +473,10 @@ class TDM extends Multiplayer {
     if (red < blue) t.color = '#FF0000';
     if (red === blue) t.color = (Math.random() < .5 ? '#FF0000' : '#0000FF');
     t.team = t.username+':LOBBY';
-    if (this.pt.length === 4) { // once four players, begin the countdown
+    if (this.pt.length === 4) { // once four players, begin the countdow
       this.readytime = Date.now();
       this.time = 60; // 1 minute starting time
+      this.global = 'Starting in '+(this.time-Math.floor((Date.now()-this.readytime)/1000));
     }
   }
 
