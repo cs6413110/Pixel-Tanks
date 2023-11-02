ef1df1fedcc593562fcb0c3e0a516a9cb10afd12
cs6413110 <129222222+cs6413110@users.noreply.github.com>
2023-11-01 18:26:43 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
+console.log('start');
+const msgpackr = require('msgpackr');
+console.log(3);
 const {multiopen, multimessage, multiclose} = require('./multiplayer.js');
 const {MongoClient} = require('mongodb');
-const msgpackr = require('msgpackr');
 const client = new MongoClient('mongodb+srv://cs641311:355608-G38@cluster0.z6wsn.mongodb.net/?retryWrites=true&w=majority');
 const tokens = new Set(), sockets = new Set();
 const valid = (token, username) => tokens.has(`${token}:${username}`);
