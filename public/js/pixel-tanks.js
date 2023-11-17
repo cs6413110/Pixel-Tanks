467e5d64c5e9fe5b394e27ee1b851920e6b8633d
CelestialKnight7 <142339732+CelestialKnight7@users.noreply.github.com>
2023-11-17 11:00:26 -0700

    fixed
    winning something still gave 100,000 crates so i fixed
    
    
    ur welcome

--- public/js/pixel-tanks.js
+++ public/js/pixel-tanks.js
@@ -1211,10 +1211,10 @@ function Game() {
             case 'gameover':
               this.implode();
               if (data.type === 'victory') {
-                Menus.menus.victory.stats = {coins: 1000, crates: 100000, xp: 50};
+                Menus.menus.victory.stats = {coins: 1000, crates: 10, xp: 50};
                 Menus.trigger('victory');
               } else {
-                Menus.menus.defeat.stats = {coins: 200, crates: 100000, xp: 10};
+                Menus.menus.defeat.stats = {coins: 200, crates: 2, xp: 10};
                 Menus.trigger('defeat');
               }
               break;
