c6012c45df8e8efb3176054185df69c1821bac03
cs6413110 <129222222+cs6413110@users.noreply.github.com>
2023-11-27 07:44:23 -0700

    Update and rename play.js to pixel-tanks.js

--- public/js/pixel-tanks.js
+++ public/js/pixel-tanks.js
@@ -1,4 +1,4 @@
-['msgpackr', 'pathfinding', 'engine', 'pixel-tanks'].forEach(url => {
+['msgpackr', 'pathfinding', 'engine', 'client'].forEach(url => {
   const script = document.createElement('SCRIPT');
   script.src = 'https://cs6413110.github.io/Pixel-Tanks/public/js/'+url+'.js';
   document.head.appendChild(script);
