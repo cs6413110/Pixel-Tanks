a2dc9df92dc1d0d62e2c0aec7baf86ac40b21c37
cs6413110 <129222222+cs6413110@users.noreply.github.com>
2023-11-28 09:51:33 -0700

    Update pixel-tanks.js

--- public/js/pixel-tanks.js
+++ public/js/pixel-tanks.js
@@ -2,7 +2,8 @@ const key = ['msgpackr', 'pathfinding', 'engine', 'client'];
 const loadNext = i => {
   const script = document.createElement('SCRIPT');
   script.src = 'https://cs6413110.github.io/Pixel-Tanks/public/js/'+key[i]+'.js';
-  if (key.length < i+1) script.onload = () => loadNext(i+1);
+  if (i+1 < key.length) script.onload = () => loadNext(i+1);
   document.head.appendChild(script);
+  alert('added script with id of '+script.src);
 }
 loadNext(0);
