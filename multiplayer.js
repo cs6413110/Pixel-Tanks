84d49541a04072c324290dffc246b1d23311bddc
cs6413110 <129222222+cs6413110@users.noreply.github.com>
2023-12-05 08:51:01 -0700

    fix

--- multiplayer.js
+++ multiplayer.js
@@ -197,7 +197,7 @@ class FFA extends Multiplayer {
   } 
 
   ondeath(t, m) {
-    this.logs.push({m: this.deathMsg(t.username, m.username, c: '#FF8C00'});
+    this.logs.push({m: this.deathMsg(t.username, m.username), c: '#FF8C00'});
     if (m.socket) m.socket.send({event: 'kill'});
     for (const ai of this.ai) if (getUsername(ai.team) === t.username) this.ai.splice(this.ai.indexOf(ai), 1);
     if (t.username) {
