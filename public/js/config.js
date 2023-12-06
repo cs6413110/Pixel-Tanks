dc48c7cda21a135855f3e330a1f531f2c8e8b5ff
BreadSpam <143108218+BreadSpam@users.noreply.github.com>
2023-12-06 09:48:51 -0700

    renamded a cosmetic

--- public/js/config.js
+++ public/js/config.js
@@ -68,7 +68,7 @@ const images = {
     'Yellow Hoodie': '/cosmetics/yellow_hoodie',
     'X': '/cosmetics/x',
     'Sweat': '/cosmetics/sweat',
-    'ShieldGold': '/cosmetics/ShieldGold',
+    'Gold Shield': '/cosmetics/gold_shield',
     'Spirals': '/cosmetics/spirals',
     'Spikes': '/cosmetics/spikes',
     'Rudolph': '/cosmetics/rudolph',
