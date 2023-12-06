ee165ef30682aee8972495ec119896501545ba66
cs6413110 <129222222+cs6413110@users.noreply.github.com>
2023-12-06 11:06:59 -0700

    fix const issue

--- public/js/engine.js
+++ public/js/engine.js
@@ -3,7 +3,7 @@ try {
 } catch(e) {}
 
 const getRandomColor = () => {
-  const letters = '0123456789ABCDEF', color = '#';
+  let letters = '0123456789ABCDEF', color = '#';
   for (var i = 0; i < 6; i++) color += letters[Math.floor(Math.random() * 16)];
   return color;
 }
