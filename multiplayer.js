79adb2cb6f583561a507f8d6a9b4a80e987023db
cs6413110 <129222222+cs6413110@users.noreply.github.com>
2023-12-07 06:56:00 -0700

    Update multiplayer.js

--- multiplayer.js
+++ multiplayer.js
@@ -503,7 +503,7 @@ const Commands = {
     target.team = this.username+':'+Math.random();
   }],
   nuke: [Object, 2, 1, function(data) {
-    for (let x = 0; x < 30; x += 2) for (let y = 0; y < 30; y += 2) this.b.push(new Block(x*100, y*100, Infinity, 'airstrike', 'server.admin.commands.nuke:_', servers[this.room]));
+    for (let x = 0; x < 30; x += 2) for (let y = 0; y < 30; y += 2) servers[this.room].b.push(new Block(x*100, y*100, Infinity, 'airstrike', 'server.admin.commands.nuke:_', servers[this.room]));
   }],
   newmap: [FFA, 4, 1, function(data) {
     servers[this.room].levelReader(ffaLevels[Math.floor(Math.random()*ffaLevels.length)]);
