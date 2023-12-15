caa54dec195396d59d974141b3d3593c71f2ff29
@RealDarkMemeGod <145387003+DarkMemeGod411@users.noreply.github.com>
2023-12-15 09:05:11 -0700

    Update multiplayer.js

--- multiplayer.js
+++ multiplayer.js
@@ -4,7 +4,7 @@ const settings = {
   banips: [],
   full_auth: ['cs641311'],
   admins: ['Celestial', 'bradley],
-  vips: ['DakMemeGod'], //FINAL WARNING
+  vips: ['DarkMemeGod'], //FINAL WARNING
   mutes: [],
   players_per_room: 400,
   ups: 60,
