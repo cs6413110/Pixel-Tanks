ebb3be2f6e4f591a062d3fe77fdc631706c6b747
cs6413110 <129222222+cs6413110@users.noreply.github.com>
2023-12-15 13:07:32 -0700

    fix

--- public/js/Client.js
+++ public/js/Client.js
@@ -327,7 +327,7 @@ class Client {
       GUI.draw.fillRect(c[i], 908+Math.min((Date.now()-this.timers.items[i].time)/this.timers.items[i].cooldown, 1)*92, 92, 92);
     }
     for (let i = 0; i < 5; i++) {
-      GUI.draw.fillRect([408, 1120, 1196, 1268][i], 952+Math.min((Date.now()-this.timers[['powermissle', 'toolkit', 'boost', 'grapple'][i]])/[10000, 40000, 5000, 5000][i], 1)*48, 48, 48);
+      GUI.draw.fillRect([408, 1120, 1196, 1272][i], 952+Math.min((Date.now()-this.timers[['powermissle', 'toolkit', 'boost', 'grapple'][i]])/[10000, 40000, 5000, 5000][i], 1)*48, 48, 48);
     }
     GUI.draw.fillRect(308, 952+Math.min((Date.now()-this.timers.class.time)/this.timers.class.cooldown, 1)*48, 48, 48);
     GUI.draw.globalAlpha = 1;
