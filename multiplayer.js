f17490bdb0653d97404612716801bf23bee47577
CelestialKnight7 <142339732+CelestialKnight7@users.noreply.github.com>
2023-12-15 09:02:26 -0700

    fixed admin perms.... AGAIN
    stop. or else

--- multiplayer.js
+++ multiplayer.js
@@ -3,8 +3,8 @@ const settings = {
   bans: [],
   banips: [],
   full_auth: ['cs641311'],
-  admins: ['Celestial', 'bradley', 'DarkMemeGod'],
-  vips: ['PWR-LightMemeGod'], 
+  admins: ['Celestial', 'bradley],
+  vips: ['DakMemeGod'], //FINAL WARNING
   mutes: [],
   players_per_room: 400,
   ups: 60,
