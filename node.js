381bfae85fb3d78c0481837c95f6187b2fe9c900
cs6413110 <129222222+cs6413110@users.noreply.github.com>
2023-12-15 09:11:53 -0700

    Update node.js

--- node.js
+++ node.js
@@ -40,6 +40,8 @@ let db;
 
 const server = http.createServer((req, res) => {
   if (req.url.includes('/download')) {
+    res.setHeader('Content-Type', 'octet-stream');
+    res.setHeader('Content-Disposition', 'attachment');
     ytdl(`https://youtube.com/watch?v=${req.url.replace('/download', '')}`).pipe(res);
   } else {
     res.end(fs.readFileSync('./public/js/pixel-tanks.js'));
