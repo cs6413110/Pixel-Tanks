d4e12374530381b2588c8790cecc594fadc43d44
cs6413110 <129222222+cs6413110@users.noreply.github.com>
2023-12-19 08:35:38 -0700

    Multi-cosmetic selecting #51
    - Let players choose which layer each cosmetic goes to
    - Should seamlessly work with database

--- public/js/PixelTanks.js
+++ public/js/PixelTanks.js
@@ -441,7 +441,9 @@ class PixelTanks {
               for (let i = 0; i < 16; i++) {
                 if (Engine.collision(x, y, 0, 0, 598+(i%4)*108, 298+Math.floor(i/4)*108, 88, 88)) {
                   if (e.button === 0) {
-                    PixelTanks.userData.cosmetic = PixelTanks.userData.cosmetics[this.cosmeticMenu*16+i];
+                    if (prompt('Do you want this cosmetic to be top layer?')) PixelTanks.userData.cosmetic_hat = PixelTanks.userData.cosmetics[this.cosmeticMenu*16+i];
+                    if (prompt('Do you want this cosmetic to be middle layer?)) PixelTanks.userData.cosmetic = PixelTanks.userData.cosmetics[this.cosmeticMenu*16+i];
+                    if (prompt('Do you want this cosmetic to be base layer?')) PixelTanks.userData.cosmetic_body = PixelTanks.userData.cosmetics[this.cosmeticMenu*16+i];
                   } else {
                     PixelTanks.userData.cosmetics.splice(this.cosmeticMenu*16+i, 1);
                   }
