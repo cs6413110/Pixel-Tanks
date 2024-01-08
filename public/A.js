f5427b4c5ba1b0c2498e5c94cb74bce0bf3db81c
cs6413110 <129222222+cs6413110@users.noreply.github.com>
2024-01-08 13:36:16 -0700

    *saving this temp since i lost it last time :'(

---
+++ public/A.js
@@ -0,0 +1,30 @@
+class A {
+  static objTemplates = {'arr': [[], a => {a.length = 0}]};
+  static classTemplates = {};
+  static createTemplate(n, v, r, p) {
+    const isClass = /^\s*class\s+/.test(e.toString());
+    A[(isClass ? 'class' : 'obj')+'Templates'][n] = isClass ? v : [v, r];
+    A[n] = [];
+    for (let i = 0; i < p; i++) A[(isClass ? 'class' : 'obj')+'Template'](n);
+  }
+  static objTemplate(n) {
+    if (!A[n].length) {
+      let e = new A.classTemplates[n]();
+      e.release = () => {
+        e.reset();
+        A[n].push(e);
+      };
+      return e;
+    } else return A[n].shift();
+  }
+  static classTemplate(n) {
+    if (!A[n].length) {
+      let e = new A.classTemplates[n]();
+      e.release = () => {
+        e.reset();
+        A[n].push(e);
+      };
+      return e;
+    } else return A[n].shift();
+  }
+}
