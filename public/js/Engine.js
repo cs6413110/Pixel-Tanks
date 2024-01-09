602aaf125e8ef814441b19ede922a5698ca66881
cs6413110 <129222222+cs6413110@users.noreply.github.com>
2024-01-09 14:50:04 -0700

    Update Engine.js

--- public/js/Engine.js
+++ public/js/Engine.js
@@ -185,7 +185,7 @@ class Engine {
         this.cells[y][x] = new Set();
       }
     }
-    const key = { 'B5': ['void', Infinity], 'B4': ['barrier', Infinity], 'B3': ['gold', 300], 'B2': ['strong', 200], 'B1': ['weak', 100]};
+    const key = {'B5': ['void', Infinity], 'B4': ['barrier', Infinity], 'B3': ['gold', 300], 'B2': ['strong', 200], 'B1': ['weak', 100]};
     for (let l = 0; l < level.length; l++) {
       for (let q = 0; q < level[l].length; q++) {
         const e = level[l][q];
