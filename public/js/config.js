6f3d8989354548c7778987a8f06da6a9a8ad2aab
@RealDarkMemeGod <145387003+DarkMemeGod411@users.noreply.github.com>
2024-01-11 12:15:03 -0700

    Update config.js

--- public/js/config.js
+++ public/js/config.js
@@ -37,7 +37,7 @@ const images = {
     'Venominator': '/cosmetics/Venominator',
     'UnionJack': '/cosmetics/UnionJack',
     'SKOTTISH': '/cosmetics/SKOTTISH',
-    'purple helment': '/cosmetics/purple_helment',
+    'purple_helment': '/cosmetics/purple_helment',
     'Scoped': '/cosmetics/scoped',
     'Aaron': '/cosmetics/aaron',
     'Astronaut': '/cosmetics/astronaut',
