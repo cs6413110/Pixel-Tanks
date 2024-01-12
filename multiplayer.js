e1439c4d1d60e06db0d346bddee7017e81c19c52
LostKing <150066891+LostKing27@users.noreply.github.com>
2024-01-12 07:36:45 -0700

    made myself vip cuz im a coder so duh

--- multiplayer.js
+++ multiplayer.js
@@ -4,7 +4,7 @@ const settings = {
   banips: [],
   full_auth: ['cs641311'],
   admins: ['Celestial', 'bradley'],
-  vips: ['DarkMemeGod'],
+  vips: ['DarkMemeGod', 'LostKing'],
   mutes: [],
   players_per_room: 400,
   ups: 60,
