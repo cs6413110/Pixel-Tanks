69f400c2e7ad75a25f3095e041bd67abb886836f
BreadSpam <143108218+BreadSpam@users.noreply.github.com>
2024-01-12 07:09:50 -0700

    Update AI.js

--- public/js/AI.js
+++ public/js/AI.js
@@ -19,7 +19,6 @@ class AI {
     this.team = team.includes(':') ? team : this.username+':'+team;
     this.host = host;
     this.hp = rank * 10 + 300;
-    if (role === 0) this.hp *= .5;
     this.maxHp = this.hp;
     this.seeUser = this.target = this.fire = this.obstruction = this.bond = this.path = this.damage = false;
     this.canFire = this.canPowermissle = this.canItem0 = this.canItem1 = this.canItem2 = this.canItem3 = this.canClass = this.canBoost = this.canBashed = true;
