444d56f65e604ec900187eefa41ea3a5d94af080
cs6413110 <129222222+cs6413110@users.noreply.github.com>
2024-01-18 12:48:13 -0700

    xif

--- public/js/Engine.js
+++ public/js/Engine.js
@@ -103,7 +103,7 @@ class Engine {
         }
       }
     } else if (a === 'turret') {
-      this.ai.push(new AI(Math.floor(t.x / 100) * 100 + 10, Math.floor(t.y / 100) * 100 + 10, 0, t.rank, 0), t.team, this));
+      this.ai.push(new AI(Math.floor(t.x / 100) * 100 + 10, Math.floor(t.y / 100) * 100 + 10, 0, t.rank, t.team, this));
       let turrets = 0;
       for (const ai of this.ai.reverse()) if (ai.role === 0 && Engine.getUsername(ai.team) === t.username && ++turrets > 3) ai.destroy();
     } else if (a === 'buff') {
