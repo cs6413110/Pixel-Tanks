224332115243129f1309ad0c203493bff66ac9ee
cs6413110 <129222222+cs6413110@users.noreply.github.com>
2024-01-18 11:54:22 -0700

    Add killai and killall commands #137

--- multiplayer.js
+++ multiplayer.js
@@ -587,7 +587,13 @@ const Commands = {
     }
   }],
   kill: [FFA, 2, 2, function(data) {
-    for (const server of Object.values(servers)) for (const t of server.pt) if (data[1] === t.username) t.damageCalc(t.x, t.y, 6000, this.username);
+    for (const server of Object.values(servers)) for (const t of server.pt) if (data[1] === t.username) for (let i = 0; i < 2; i++) t.damageCalc(t.x, t.y, 6000, this.username);
+  }],
+  killall: [Object, 2, 1, function(data) {
+    for (const t of servers[this.room].pt) for (let i = 0; i < 2; i++) t.damageCalc(t.x, t.y, 6000, this.username)
+  }],
+  killai: [Object, 2, 1, function(data) {
+    for (const ai of servers[this.room].ai.reverse()) ai.destroy();
   }],
   ai: [Object, 2, 7, function(data) {
     for (let i = 0; i < Number(data[5]); i++) servers[this.room].ai.push(new AI(Math.floor(Number(data[1]) / 100) * 100 + 10, Math.floor(Number(data[2]) / 100) * 100 + 10, Number(data[3]), Math.min(20, Math.max(0, Number(data[4]))), data[6], servers[this.room]));
