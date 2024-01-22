6f450ba7f15367f2485138ebfc55bb4c14a90475
cs6413110 <129222222+cs6413110@users.noreply.github.com>
2024-01-22 12:26:54 -0700

    Builder spam attempt

--- public/js/Client.js
+++ public/js/Client.js
@@ -641,7 +641,11 @@ class Client {
         this.timers.class = {time: Date.now(), cooldown: 25000};
       } else if (c === 'builder') {
         this.tank.use.push('turret');
-        this.timers.class = {time: Date.now(), cooldown: 25000};
+        this.timers.class = {time: Date.now(), cooldown: 50}
+        if (!this.turretSpam) this.turretSpam = setTimeout(() => {
+          this.turretSpam = undefined;
+          this.timers.class = {time: Date.now(), cooldown: 25000};
+        }, 10000);
       } else if (c === 'warrior') {
         this.tank.use.push('buff');
         this.timers.class = {time: Date.now(), cooldown: 40000};
