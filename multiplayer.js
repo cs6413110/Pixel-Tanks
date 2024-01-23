fe60e3c8c81123b962661bc332ad024882fcc0f5
cs6413110 <129222222+cs6413110@users.noreply.github.com>
2024-01-23 11:33:45 -0700

    no

--- multiplayer.js
+++ multiplayer.js
@@ -617,11 +617,11 @@ const Commands = {
     for (const t of servers[this.room].pt) for (let i = 0; i < 2; i++) t.damageCalc(t.x, t.y, 6000, this.username);
   }],
   killai: [Object, 2, 1, function(data) {
-    for (const ai of servers[this.room].ai.reverse()) ai.destroy();
+    for (const ai of servers[this.room].ai) ai.destroy();
   }],
-  ai: [Object, 2, 7, function(data) {
+  /*ai: [Object, 2, 7, function(data) {
     for (let i = 0; i < Number(data[5]); i++) servers[this.room].ai.push(new AI(Math.floor(Number(data[1]) / 100) * 100 + 10, Math.floor(Number(data[2]) / 100) * 100 + 10, Number(data[3]), Math.min(20, Math.max(0, Number(data[4]))), data[6], servers[this.room]));
-  }],
+  }],*/
   spectate: [Object, 3, 2, function(data) {
     for (const server of Object.values(servers)) for (const t of server.pt) if (t.username === data[1]) t.ded = true;
   }],
