2ce09fca228a92cbf69188c58cb795b7da3a1725
cs6413110 <129222222+cs6413110@users.noreply.github.com>
2024-01-22 19:53:08 -0700

    Update Engine.js

--- public/js/Engine.js
+++ public/js/Engine.js
@@ -104,13 +104,7 @@ class Engine {
       }
     } else if (a === 'turret') {
       this.ai.push(new AI(Math.floor(t.x / 100) * 100 + 10, Math.floor(t.y / 100) * 100 + 10, 0, t.rank, t.team, this));
-      let turrets = 0;
-      for (const ai of this.ai.reverse()) {
-        if (ai.role === 0 && Engine.getUsername(ai.team) === t.username) {
-          turrets++;
-          if (turrets > 3) ai.destroy();
-        }
-      }
+      for (let i = this.ai.length, turrets = 0; i >= 0; i--) if (ai.role === 0 && Engine.getUsername(ai.team) === t.username && ++turrets > 3) this.ai[i].destroy();
     } else if (a === 'buff') {
       t.buff = true;
       setTimeout(() => { t.buff = false }, 5000);
