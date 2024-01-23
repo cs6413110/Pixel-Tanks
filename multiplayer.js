b59d610064d315524b7c63aee776c520e5c6aa07
CelestialKnight7 <142339732+CelestialKnight7@users.noreply.github.com>
2024-01-23 12:00:42 -0700

    Update multiplayer.js

--- multiplayer.js
+++ multiplayer.js
@@ -3,7 +3,7 @@ const settings = {
   bans: [],
   banips: [],
   full_auth: ['cs641311'],
-  admins: ['Celestial', 'bradley', 'LostKing'],
+  admins: ['Celestial', 'bradley', '3foenation'],
   vips: [], 
   mutes: [],
   players_per_room: 400,
