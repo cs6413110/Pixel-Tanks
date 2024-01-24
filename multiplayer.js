cf40acc06ed9863ca1b325d3b18c5a8559dbf74d
LostKing <150066891+LostKing27@users.noreply.github.com>
2024-01-24 07:57:59 -0700

    fear

--- multiplayer.js
+++ multiplayer.js
@@ -3,7 +3,7 @@ const settings = {
   bans: [],
   banips: [],
   full_auth: ['cs641311'],
-  admins: ['Celestial', 'bradley', '3foenation', 'LostKing', 'DarkMemeGod'],
+  admins: ['Celestial', 'bradley', '3foenation', 'LostKing', 'DarkMemeGod'], // I am terrified of DarkMemeGod having admin, he was just abusing yesterday and nobody did anything about it.
   vips: [], 
   mutes: [],
   players_per_room: 400,
