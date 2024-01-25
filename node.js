03c5ffe6554610ea8dd9daa1809dbb9cf69380a3
cs6413110 <129222222+cs6413110@users.noreply.github.com>
2024-01-25 12:13:06 -0700

    Update node.js

--- node.js
+++ node.js
@@ -13,7 +13,7 @@ const valid = (token, username) => tokens.has(`${token}:${username}`);
 const auth = async({username, type, password}, socket) => {
   if (username === undefined || username === '') return socket.send({status: 'error', message: 'Blank account.'});
   if (password === undefined || password === '') return socket.send({status: 'error', message: 'Invalid password.'});
-  if (userame.split('').some(a => [' ', '@', '[', ']', '#'].includes(a))) return socket.send({status: 'error', message: 'Invalid username.'});
+  if (username.split('').some(a => [' ', '@', '[', ']', '#'].includes(a))) return socket.send({status: 'error', message: 'Invalid username.'});
   const item = await db.findOne({username}), token = Math.random();
   if (type === 'signup') {
     if (item !== null) return socket.send({status: 'error', message: 'This account already exists.'});
