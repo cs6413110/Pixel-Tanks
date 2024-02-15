086d6201f2385187df41132f8579107da90c747e
BreadSpam <143108218+BreadSpam@users.noreply.github.com>
2024-02-15 11:35:51 -0700

    Update Engine.js

--- public/js/Engine.js
+++ public/js/Engine.js
@@ -69,7 +69,7 @@ class Engine {
       }
     } else if (a === 'break') {
       for (const cell of t.cells) {
-        const c = cell.split('x'), cx = c[0], cy = c[1], breakable = ['gold', 'weak', 'strong', 'spike', 'barrier', 'void', 'barrel'];
+        const c = cell.split('x'), cx = c[0], cy = c[1], breakable = ['gold', 'weak', 'strong', 'spike', 'barrier', 'void', 'barrel', 'halfbarrier'];
         for (const entity of this.cells[cx][cy]) if (entity instanceof Block && Engine.collision(t.x, t.y, 80, 80, entity.x, entity.y, 100, 100) && breakable.includes(entity.type)) entity.destroy();
       }
     } else if (a === 'bomb') {
