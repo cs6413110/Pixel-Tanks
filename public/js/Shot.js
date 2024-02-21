45eb27c8b3f515732b6ee688177caf16d4dc38fd
cs6413110 <129222222+cs6413110@users.noreply.github.com>
2024-02-21 11:07:13 -0700

    Hit Priority #128

--- public/js/Shot.js
+++ public/js/Shot.js
@@ -1,226 +1,100 @@
 class Shot {
-  constructor(x, y, xm, ym, type, rotation, team, rank, host) {
-    this.team = team;
-    this.r = rotation;
-    this.type = type;
-    this.host = host;
-    this.e = Date.now();
-    this.raw = {};
-    this.id = Math.random();
-    this.md = this.damage = Shot.settings.damage[type]*(rank*10+300)/500;
-    const factor = 6/Math.sqrt(xm**2+ym**2);
-    this.xm = xm*factor*Shot.settings.speed[type];
-    this.ym = ym*factor*Shot.settings.speed[type];
-    const data = Shot.calc(x, y, xm, ym);
-    this.sx = this.x = data.x-5;
-    this.sy = this.y = data.y-5;
+  static settings = {bullet: [20, 6], shotgun: [20, 4.8], grapple: [0, 12], powermissle: [100, 9, 50], megamissle: [200, 9, 100], healmissle: [-150, 9, 99]};
+  static args = ['x', 'y', 'r', 'type', 'team', 'rank', 'host'];
+  static raw = ['team', 'r', 'type', 'x', 'y', 'sx', 'sy', 'id'];
+  constructor() {
+    for (const p of Shot.raw) Object.defineProperty(this, p, {get: () => this.raw[p], set: v => this.setValue(p, v), configurable: true});
     this.cells = new Set();
-    for (let dx = this.x/100, dy = this.y/100, i = 0; i < 4; i++) {
-      const cx = Math.max(0, Math.min(29, Math.floor(i < 2 ? dx : dx+.09))), cy = Math.max(0, Math.min(29, Math.floor(i % 2 ? dy : dy+.09)));
-      host.cells[cx][cy].add(this);
-      this.cells.add(cx+'x'+cy);
-    }
-    this.u();
   }
-
-  static settings = {
-    damage: {
-      bullet: 20,
-      shotgun: 20,
-      grapple: 10,
-      powermissle: 100,
-      megamissle: 200,
-      healmissle: -200,
-      dynamite: 0,
-      fire: 0,
-      usb: 0,
-    },
-    speed: {
-      bullet: 1,
-      shotgun: .8,
-      grapple: 2,
-      powermissle: 1.5,
-      megamissle: 1.5,
-      healmissle: 1.5,
-      dynamite: .8,
-      fire: .9,
-      usb: .8,
-    },
-    size: {
-      healmissle: 99,
-      powermissle: 50,
-      megamissle: 100,
-    }
-  }
-
-  static calc(x, y, xm, ym) {
-    const r = 70;
-    const a = xm === 0 ? 1000000 : ym / xm;
-    const b = xm === 0 ? 0 : (a > 0 ? -1 : 1);
-    const c = Math.sqrt(r**2+(r*a)**2);
-    const d = r*c;
-    const cx = -r*b*d/c**2;
-    const cy = Math.abs(r*a)*d/c**2;
-    return {x: x+cx*(ym >= 0 ? 1 : -1), y: y+cy*(ym >= 0 ? 1 : -1)};
-  }
-
-  collision() {
-    const { host, x, y, type, cells} = this;
-    if (x < 0 || x > 3000 || y < 0 || y > 3000) {
-      if (type === 'grapple') {
-        const t = host.pt.find(t => t.username === Engine.getUsername(this.team));
-        if (t.grapple) t.grapple.bullet.destroy();
-        t.grapple = { target: { x: x, y: y }, bullet: this };
-        this.update = () => {};
-        return false;
-      } else if (type === 'dynamite') {
-        this.update = () => {}
-        return false;
-      } else {
-        if (Shot.settings.size[type]) host.d.push(new Damage(x - Shot.settings.size[type] / 2 + 10, y - Shot.settings.size[type] / 2 + 10, Shot.settings.size[type], Shot.settings.size[type], this.damage, this.team, host));
-        return true;
+  init(x, y, r, type, team, rank, host) {
+    for (const i in Shot.args) this[Shot.args[i]] = arguments[i];
+    this.e = Date.now();
+    this.id = Math.random();
+    this.md = this.damage = Shot.settings[this.type][0]*(rank*10+300)/500;
+    this.x = this.sx = (this.xm = Math.cos(r)*Shot.settings[this.type][1])*11.66;
+    this.y = this.sy = (this.ym = Math.sin(r)*Shot.settings[this.type][1])*11.66;
+    for (let x = Math.max(0, Math.min(29, Math.floor(this.x/100)); x <= Math.max(0, Math.min(29, Math.floor((this.x+10)/100))); x++) {
+      for (let y = Math.max(0, Math.min(29, Math.floor(this.y/100)); y <= Math.max(0, Math.min(29, Math.floor((this.y+10)/100))); y++) {
+        host.cells[x][y].add(this);
+        this.cells.add(`${x}x${y}`);
       }
     }
-    for (const cell of cells) { 
-      const [cx, cy] = cell.split('x');
-      for (const e of host.cells[cx][cy]) {
-        if (e instanceof Tank) {
-          if (e.ded || !Engine.collision(x, y, 10, 10, e.x, e.y, 80, 80)) continue;
-          if (type === 'grapple') {
-            if (e.grapple) e.grapple.bullet.destroy();
-            e.grapple = {target: host.pt.find(tank => tank.username === Engine.getUsername(this.team)), bullet: this};
-            this.target = e;
-            this.offset = [e.x-x, e.y-y];
-            this.update = this.dynaUpdate;
-            return false;
-          } else if (type === 'dynamite' || type === 'usb') {
-            this.target = e;
-            this.offset = [e.x-x, e.y-y];
-            this.update = this.dynaUpdate;
-            if (type === 'usb') setTimeout(() => this.destroy(), 20000);
-            return false;
-          } else if (type === 'fire') {
-            if (e.immune) return true;
-            if (e.fire) clearTimeout(e.fireTimeout);
-            e.fire = { team: this.team, frame: e.fire?.frame || 0 };
-            e.fireInterval ??= setInterval(() => e.fire.frame ^= 1, 50); // OPTIMIZE make gui effects render by date time not by server interval
-            e.fireTimeout = setTimeout(() => {
-              clearInterval(e.fireInterval);
-              e.fire = false;
-            }, 4000);
-            return true;
-          } else {
-            if (Shot.settings.size[type]) {
-              host.d.push(new Damage(x - Shot.settings.size[type] / 2 + 10, y - Shot.settings.size[type] / 2 + 10, Shot.settings.size[type], Shot.settings.size[type], this.damage, this.team, host));
-            } else if (Engine.getTeam(e.team) !== Engine.getTeam(this.team)) {
-              e.damageCalc(x, y, this.damage, Engine.getUsername(this.team));
-            }
-            return true;
-          }
-        } else if (e instanceof Block) {
-          if (!e.c || !Engine.collision(e.x, e.y, 100, 100, x, y, 10, 10)) continue;
-          if (type === 'grapple' || type === 'dynamite') {
-            if (type === 'grapple') {
-              const t = this.host.pt.find(t => t.username === Engine.getUsername(this.team));
-              if (t.grapple) t.grapple.bullet.destroy();
-              t.grapple = {target: e, bullet: this}
-            }
-            this.update = () => {};
-            return false;
-          } else {
-            if (type === 'fire') host.b.push(A.template('Block').init(e.x, e.y, Infinity, 'fire', this.team, host));
-            if (Shot.settings.size[type]) {
-              host.d.push(new Damage(x - Shot.settings.size[type] / 2 + 10, y - Shot.settings.size[type] / 2 + 10, Shot.settings.size[type], Shot.settings.size[type], this.damage, this.team, host));
-            } else if (type !== 'fire') {
-              e.damage(this.damage);
-            }
-            return true;
-          }
-        } else if (e instanceof AI) {
-          if (!Engine.collision(x, y, 10, 10, e.x, e.y, 80, 80)) continue;
-          if (type === 'dynamite' || type === 'usb') {
-            this.target = e;
-            this.offset = [e.x-x, e.y-y];
-            this.update = this.dynaUpdate;
-            if (type === 'usb') setTimeout(() => this.destroy(), 15000);
-            return false;
-          } else if (type === 'fire') {
-            if (e.fire) clearTimeout(e.fireTimeout);
-            e.fire = {team: this.team, frame: e.fire?.frame || 0};
-            e.fireInterval ??= setInterval(() => e.fire.frame ^= 1, 50);
-            e.fireTimeout = setTimeout(() => {
-              clearInterval(e.fireInterval);
-              e.fire = false;
-            }, 4000);
-            return true;
-          } else {
-            if (Shot.settings.size[type]) {
-              host.d.push(new Damage(x - Shot.settings.size[type] / 2 + 10, y - Shot.settings.size[type] / 2 + 10, Shot.settings.size[type], Shot.settings.size[type], this.damage, this.team, host));
-            } else if (Engine.getTeam(e.team) !== Engine.getTeam(this.team)) {
-              e.damageCalc(x, y, this.damage, Engine.getUsername(this.team));
-            }
-            return true;
-          }
-        }
+    return this;
+  }
+  collide(e) {
+    let size = Shot.settings[this.type][2], o = size/2+10, isBlock = e instanceof Block, pullGrapple = (isBlock || !e) && this.type === 'grapple';
+    if (size) this.host.d.push(new Damage(this.x-o, this.y-o, size, size, this.damage, this.team, this.host)); // damage change to square instead of rect hitbox?
+    if (this.type === 'dynamite' || this.type === 'usb' || this.type === 'grapple') {
+      const g = pullGrapple ? this.host.pt.find(t => t.username === Engine.getUsername(this.team)) : e;
+      this.target = g;
+      this.offset = [g.x-this.x, g.y-this.y];
+      if (pullGrapple) this.update = () => {};
+      if (this.type === 'grapple') {
+        if (g.grapple) g.grapple.bullet.destroy();
+        g.grapple = {target: pullGrapple ? {x: e.x, y: e.y} : this.host.pt.find(t => t.username === Engine.getUsername(this.team)), bullet: this};
+      }
+      return false;
+    } else if (this.type === 'fire') {
+      if (isBlock) return this.host.b.push(A.template('Block').init(e.x, e.y, Infinity, 'fire', this.team, this.host));
+      if (!e.immune) e.fire = {team: this.team, time: Date.now()};
+    } else if (size) {
+      this.host.d.push(new Damage(x-o, y-o, size, size, this.damage, this.team, this.host));
+    } else if (Engine.getTeam(e.team) !== Engine.getTeam(this.team) && e) {
+      if (isBlock) e.damage(this.damage); else e.damageCalc(this.x, this.y, this.damage, Engine.getUsername(this.team));
+    }
+    return true;
+  }
+  score(e) {
+    if (e instanceof Block) return 1;
+    if (e instanceof AI) return Engine.getTeam(e.team) === Engine.getTeam(this.team) || this.type === 'grapple' ? 5 : 2;
+    if (e instanceof Tank) return (this.type === 'grapple' ? 0 : 1)+(Engine.getTeam(e.team) === Engine.getTeam(this.team) ? 4 : 3);
+  }
+  collision() {
+    if (this.x < 0 || this.y < 0 || this.x > 3000 || this.y > 3000) this.collide();
+    for (const cell of this.cells) {
+      const c = cell.split('x');
+      for (const e of [...this.host.cells[c[0]][c[1]]].sort((a, b) => this.score(b) - this.score(a))) {
+        let size = e instanceof Block || e.role === 0 ? 100 : 80;
+        if (!e.ded && !e.c && Engine.collision(this.x, this.y, 10, 10, e.x, e.y, size, size)) return this.collide(e);
       }
     }
     return false;
   }
-
-  dynaUpdate() {
-    this.oldx = this.x;
-    this.oldy = this.y;
-    this.x = this.target.x - this.offset[0];
-    this.y = this.target.y - this.offset[1];
-    this.cellUpdate();
-    this.u();
-    if (this.target.ded) this.destroy();
-    if (this.host.pt.find(t => t.username === Engine.getUsername(this.team))?.ded) this.destroy();
-  }
-
-  cellUpdate() {
-    if (Math.floor(this.oldx/100) !== Math.floor(this.x/100) || Math.floor(this.oldy/100) !== Math.floor(this.y/100) || Math.floor((this.oldx+10)/100) !== Math.floor((this.x+10)/100) || Math.floor((this.oldy+10)/100) !== Math.floor((this.y+10)/100)) { 
-      const cells = new Set();
-      for (let dx = this.x/100, dy = this.y/100, i = 0; i < 4; i++) {
-        const cx = Math.max(0, Math.min(29, Math.floor(i < 2 ? dx : dx + .09))), cy = Math.max(0, Math.min(29, Math.floor(i % 2 ? dy : dy + .09)));
-        this.host.cells[cx][cy].add(this);
-        cells.add(cx+'x'+cy);
-      }
-      for (const cell of [...this.cells].filter(c => !cells.has(c))) {
-        const [x, y] = cell.split('x');
-        this.host.cells[x][y].delete(this);
-      }
-      this.cells = cells;
-    }
-  }
-
   update() {
-    const time = Math.floor((Date.now()-this.e)/5);
-    this.oldx = this.x;
-    this.oldy = this.y;
-    this.x = time*this.xm+this.sx;
-    this.y = time*this.ym+this.sy;
-    this.cellUpdate();
-    if (this.collision()) this.destroy();
+    const time = Math.floor((Date.now()-this.e)/5), x = this.target?.x || time*this.xm+this.sx, y = this.target?.y || time*this.ym+this.sy, x1 = 0|(x/100), x2 = 0|((x+10)/100), y1 = 0|(y/100), y2 = 0|((y+10)/100);
+    if (0|(this.x/100) !== x1 || 0|(this.y/100) !== y2 || 0|((this.x+10)/100) !== x2 || 0|((this.y+10)/100) !== y2) {
+      del: for (const cell of this.cells) {
+        let c = cell.split('x'), xv = c[0], yv = c[1];
+        for (let x = x1; x < x2; x++) for (let y = y1; y < y2; y++) if (x === xv && y === yv) continue del;
+        this.host.cells[xv][yv].delete(this);
+        this.cells.delete(`${xv}x${yv}`);
+      }
+      for (let x = x1; x < x2; x++) for (let y = y1; y < y2; y++) {
+        if (!this.cells.has(x+'x'+y)) {
+          this.host.cells[x][y].add(this);
+          this.cells.add(`${x}x${y}`);
+        }
+      }
+    }
+    this.x = x;
+    this.y = y;
+    if (this.collision()) return this.destroy();
+    if (this.target?.ded || this.host.pt.find(t => t.username === Engine.getUsername(this.team))?.ded) return this.destroy();
     if (this.type === 'shotgun') {
-      this.d = Math.sqrt((this.x - this.sx) ** 2 + (this.y - this.sy) ** 2);
-      this.damage = this.md - (this.d / 300) * this.md;  
-      if (this.d >= 300) this.destroy();
+      this.d = Math.sqrt((this.x-this.sx)**2+(this.y-this.sy)**2);
+      if (this.d >= 300) return this.destroy();
+      this.damage = (1-this.d/300)*this.md;  
     } else if (this.type === 'dynamite') this.r += 5;
-    this.u();
   }
-
-  u() {
+  setValue(p, v) {
     this.updatedLast = Date.now();
-    for (const property of ['team', 'r', 'type', 'x', 'y', 'sx', 'sy', 'id']) this.raw[property] = this[property];
+    this.raw[p] = v;
   }
-
   destroy() {
-    const index = this.host.s.indexOf(this);
-    if (index !== -1) this.host.s.splice(index, 1);
+    this.host.s.splice(this.host.s.indexOf(this), 1);
     for (const cell of this.cells) {
-      const [x, y] = cell.split('x');
-      this.host.cells[x][y].delete(this);
+      const c = cell.split('x');
+      this.host.cells[c[0]][c[1]].delete(this);
     }
   }
 }
