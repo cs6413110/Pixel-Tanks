5868bfbb20c2ac3a86a6726beacf293633276614
BreadSpam <143108218+BreadSpam@users.noreply.github.com>
2024-02-21 09:45:33 -0700

    Update Client.js

--- public/js/Client.js
+++ public/js/Client.js
@@ -236,7 +236,7 @@ class Client {
     if (Engine.getTeam(t.team) === 'RED') {
       GUI.drawText(username, t.x+40, t.y-25, 50, '#ff0000', 0.5);
     } else if (Engine.getTeam(t.team) === 'BLUE') {
-      GUI.drawText(username, t.x+40, t.y-25, 50, '#00ff00', 0.5);
+      GUI.drawText(username, t.x+40, t.y-25, 50, '#0000ff', 0.5);
     } else GUI.drawText(username, t.x+40, t.y-25, 50, '#ffffff', 0.5);
 
     if (t.buff) GUI.drawImage(PixelTanks.images.tanks.buff, t.x-5, t.y-5, 80, 80, .2);
