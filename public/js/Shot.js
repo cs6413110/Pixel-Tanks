2a9aa943212f8d058026d1a5e8efb4c84d80ae97
cs6413110 <129222222+cs6413110@users.noreply.github.com>
2024-02-21 15:21:41 -0700

    Update Shot.js

--- public/js/Shot.js
+++ public/js/Shot.js
@@ -64,7 +64,7 @@ class Shot {
   update() {
     const time = (Date.now()-this.e)/15, x = this.target?.x || time*this.xm+this.sx, y = this.target?.y || time*this.ym+this.sy, x1 = Math.floor(x/100), x2 = Math.floor((x+10)/100), y1 = Math.floor(y/100), y2 = Math.floor((y+10)/100);
     if (Math.floor(this.x/100) !== x1 || Math.floor(this.y/100) !== y2 || Math.floor((this.x+10)/100) !== x2 || Math.floor((this.y+10)/100) !== y2) {
-      console.log(x1, y2, x2, y2);
+      console.log(this.cells);
       del: for (const cell of this.cells) {
         let c = cell.split('x'), xv = c[0], yv = c[1];
         for (let x = x1; x < x2; x++) for (let y = y1; y < y2; y++) if (x === xv && y === yv) continue del;
