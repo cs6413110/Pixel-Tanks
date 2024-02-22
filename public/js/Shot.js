5de7e5131402b8abfe6adcdf8f47229f673d6508
cs6413110 <129222222+cs6413110@users.noreply.github.com>
2024-02-22 07:51:43 -0700

    offset

--- public/js/Shot.js
+++ public/js/Shot.js
@@ -62,7 +62,7 @@ class Shot {
     return false;
   }
   update() {
-    const time = Math.floor((Date.now()-this.e)/15), x = this.target ? this.target.x+this.offset[0] : time*this.xm+this.sx, y = this.target ? this.target.y+this.offset[1] : time*this.ym+this.sy, x1 = Math.floor(x/100), x2 = Math.floor((x+10)/100), y1 = Math.floor(y/100), y2 = Math.floor((y+10)/100);
+    const time = Math.floor((Date.now()-this.e)/15), x = this.target ? this.target.x-this.offset[0] : time*this.xm+this.sx, y = this.target ? this.target.y-this.offset[1] : time*this.ym+this.sy, x1 = Math.floor(x/100), x2 = Math.floor((x+10)/100), y1 = Math.floor(y/100), y2 = Math.floor((y+10)/100);
     if (x < 0 || y < 0 || x+10 >= 3000 || y+10 >= 3000) if (!this.target && this.collide()) return this.destroy();
     if (Math.floor(this.x/100) !== x1 || Math.floor(this.y/100) !== y2 || Math.floor((this.x+10)/100) !== x2 || Math.floor((this.y+10)/100) !== y2) {
       del: for (const cell of this.cells) {
