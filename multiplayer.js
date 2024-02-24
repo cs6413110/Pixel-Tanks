25141689791415314073710a53b6d766bcaa4b44
cs6413110 <129222222+cs6413110@users.noreply.github.com>
2024-02-24 12:07:50 -0700

    dont make me regret

--- multiplayer.js
+++ multiplayer.js
@@ -594,7 +594,7 @@ const Commands = {
   gpt: [Object, 4, -1, function(data) {
     gpt({prompt: data.slice(1).join(' '), model: 'gpt-4'}, (err, data) => servers[this.room].pt.find(t => t.username === this.username).privateLogs.push({m: err === null ? data.gpt : err, c: '#DFCFBE'}));
   }],
-  dalle: [Object, 2, -1, function(data) {
+  dalle: [Object, 3, -1, function(data) {
     dalle.v1({prompt: data.slice(1).join(' ')}, (err, data) => {
       for (const image of data.images) this.send({event: 'link', link: image});
     });
