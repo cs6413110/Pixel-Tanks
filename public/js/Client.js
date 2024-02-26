bf0ed1fc65e034342bc0fcce0e8d1d7798a74597
cs6413110 <129222222+cs6413110@users.noreply.github.com>
2024-02-26 10:20:37 -0700

    Max char limit client side #325

--- public/js/Client.js
+++ public/js/Client.js
@@ -448,7 +448,7 @@ class Client {
   }
 
   chat(e) {
-    if (e.key.length === 1) this.msg += e.key;
+    if (e.key.length === 1) this.msg = (this.msg+e.key).splice(0, 100);
     if (e.keyCode === 8) this.msg = this.msg.slice(0, -1);
     if (e.keyCode === 13) {
       if (this.msg !== '') {
