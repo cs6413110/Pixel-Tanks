9747bfb0d7c8aced89bd1f80396e23e044d1fb7a
cs6413110 <129222222+cs6413110@users.noreply.github.com>
2024-03-25 14:02:55 -0700

    Update multiplayer.js

--- multiplayer.js
+++ multiplayer.js
@@ -709,7 +709,7 @@ const Commands = {
   }],
   getlogs: [Object, 2, 2, function(data) {
     const logs = fs.readFileSync('log.txt').toString().split('\n').slice(1), t = servers[this.room].pt.find(t => t.username === this.username);
-    for (let i = 0; i < Math.min(logs.length, Number(data[1])), i++) t.privateLogs.push({m: logs[i], c: '#A9A9A9'});
+    for (let i = 0; i < Math.min(logs.length, Number(data[1])); i++) t.privateLogs.push({m: logs[i], c: '#A9A9A9'});
   }],
   announce: [Object, 3, -1, function(data) {
     for (const server of Object.values(servers)) server.logs.push({m: '[Announcement]['+this.username+'] '+data.slice(1).join(' '), c: '#FFF87D'});
