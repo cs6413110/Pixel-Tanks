6de5106bbe6fb19c93cb6110471490e4e0f0ae40
cs6413110 <129222222+cs6413110@users.noreply.github.com>
2024-04-05 15:34:51 -0700

    small optimizations brainhurt #420 #70

--- multiplayer.js
+++ multiplayer.js
@@ -122,16 +122,16 @@ class Multiplayer extends Engine {
   chunkload(t, x, y) {
     const w = 21, h = 15;
     const ocx = Math.floor(t.x/100)+.5, ocy = Math.floor(t.y/100)+.5, ncx = Math.floor(x/100)+.5, ncy = Math.floor(y/100)+.5;
-    let xd = ocx-ncx, yd = ocy-ncy, yda = yd < 0 ? -1 : 1, xda = xd < 0 ? -1 : 1;
-    for (let yl = false, nys = (yda > 0 ? 0 : -1)+ncy-h/2*yda, y = nys; y != nys+h*yda; y += yda) {
-      if (y === nys+Math.min(h, Math.abs(yd))*yda) yl = true;
-      for (let nxs = (xda > 0 ? 0 : -1)+ncx-w/2*xda, x = nxs; x != nxs+(yl ? Math.min(w, Math.abs(xd))*xda : w*xda); x += xda) {
+    let xd = ocx-ncx, yd = ocy-ncy, yda = yd < 0 ? -1 : 1, xda = xd < 0 ? -1 : 1, yl = Math.min(h, Math.abs(yd))*yda;
+    for (let l = false, nys = (yda > 0 ? 0 : -1)+ncy-h/2*yda, y = nys; y != nys+h*yda; y += yda) {
+      if (y === nys+yl) l = true;
+      for (let nxs = (xda > 0 ? 0 : -1)+ncx-w/2*xda, x = nxs; x != nxs+(l ? Math.min(w, Math.abs(xd)) : w)*xda; x += xda) {
         draw.fillRect(x*s+1, y*s+1, s-2, s-2);
       }
     }
-    for (let yl = false, oys = (yda > 0 ? -1 : 0)+ocy+h/2*yda, y = oys; y != oys-h*yda; y -= yda) {
-      if (y === oys-Math.min(h, Math.abs(yd))*yda) yl = true;
-      for (let oxs = (xda > 0 ? -1 : 0)+ocx+w/2*xda, x = oxs; x != oxs-(yl ? Math.abs(Math.min(w, Math.abs(xd))*xda) : w)*xda; x -= xda) {
+    for (let l = false, oys = (yda > 0 ? -1 : 0)+ocy+h/2*yda, y = oys; y != oys-h*yda; y -= yda) {
+      if (y === oys-yl) l = true;
+      for (let oxs = (xda > 0 ? -1 : 0)+ocx+w/2*xda, x = oxs; x != oxs-(l ? Math.min(w, Math.abs(xd)) : w)*xda; x -= xda) {
         draw.fillRect(x*s+1, y*s+1, s-2, s-2);
       }
     }
