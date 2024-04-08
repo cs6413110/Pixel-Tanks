26c8885d8c98103264c6e2f5b18a64b752d04599
cs6413110 <129222222+cs6413110@users.noreply.github.com>
2024-04-08 10:01:21 -0700

    updateEntity bindings for Shot

--- public/js/Shot.js
+++ public/js/Shot.js
@@ -90,8 +90,9 @@ class Shot {
     } else if (this.type === 'dynamite') this.r += 5;
   }
   setValue(p, v) {
-    this.updatedLast = Date.now();
-    this.raw[p] = v;
+    this.updatedLast = Date.now(); // REMOVE THIS SOON
+    this.raw[p] = v; // SOON UNNECCESSARY
+    this.host.updateEntity(this.id, this.x, this.y, 10, 10, p, v); // new method of update logging :)
   }
   reset = () => this.cells.clear();
   destroy() {
