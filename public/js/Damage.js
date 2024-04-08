1b583af7e8bd29840d874c1d74a24ce6cd652d84
cs6413110 <129222222+cs6413110@users.noreply.github.com>
2024-04-08 10:16:22 -0700

    Damage upgrades #70 may break game

--- public/js/Damage.js
+++ public/js/Damage.js
@@ -1,19 +1,19 @@
 class Damage {
   static args = ['x', 'y', 'w', 'h', 'a', 'team', 'host'];
-  constructor(x, y, w, h, a, team, host) {
-    for (const i in arguments) this[Damage.args[i]] = arguments[i];
-    this.raw = {};
-    this.f = 0;
-    this.id = Math.random();
+  static raw = ['x', 'y', 'w', 'h', 'f']; // x,y,w,h don't change so maybe doesn't matter if raw(for new system only).
+  constructor() {
     this.cells = new Set();
-    try {
-      for (let dx = this.x/100, dy = this.y/100, i = 0; i < 4; i++) {
+    for (const p of Damage.raw) Object.defineProperty(this, p, {get: () => this.raw[p], set: v => this.setValue(p, v), configurable: true});
+  }
+  init(x, y, w, h, a, team, host) {
+    this.raw = {};
+    this.id = Math.random();
+    for (const i in arguments) this[Damage.args[i]] = arguments[i];
+    this.f = 0;
+    for (let dx = this.x/100, dy = this.y/100, i = 0; i < 4; i++) { // upgrade this soon for more dynamic rect
       const cx = Math.max(0, Math.min(29, Math.floor(i < 2 ? dx : dx+w/100-.01))), cy = Math.max(0, Math.min(29, Math.floor(i % 2 ? dy : dy+h/100-.01)));
       host.cells[cx][cy].add(this);
       this.cells.add(cx+'x'+cy);
-    }
-    } catch(e) {
-      host.logs.push({m: 'ghost explosion with data of '+JSON.stringify(this.raw), c: '#FF0000'});
     }
     const cache = new Set();
     for (const cell of this.cells) {
@@ -31,25 +31,25 @@ class Damage {
         }
       }
     }
-    this.i = setInterval(() => {
-      this.f++;
-      this.u();
-    }, 18);
+    this.i = setInterval(() => (this.f++), 18); // remove pls me this is pain
     setTimeout(() => this.destroy(), 200);
   }
-  
-  u() {
-    this.updatedLast = Date.now();
-    for (const property of ['x', 'y', 'w', 'h', 'f', 'id']) this.raw[property] = this[property];
+  setValue(p, v) {
+    this.updatedLast = Date.now(); // replace
+    this.raw[p] = v; // replace soon
+    this.host.updateEntity(this.id, this.x, this.y, this.w, this.h, p, v); // new bindings
   }
-
+  reset() {
+    // loop through unnecessary and set to undefined? Or maybe it doesn't matter since it will be auto reset on recycle?
+    this.cells.clear();
+  }
   destroy() {
     clearInterval(this.i);
-    const index = this.host.d.indexOf(this);
-    if (index !== -1) this.host.d.splice(index, 1);
+    this.host.d.splice(this.host.d.indexOf(this), 1);
     for (const cell of this.cells) {
       const [x, y] = cell.split('x');
       this.host.cells[x][y].delete(this);
     }
+    this.release();
   }
 }
