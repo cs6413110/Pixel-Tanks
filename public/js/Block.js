1d8143948f19e8ea0aa4561eb25b396dd4f9b704
cs6413110 <129222222+cs6413110@users.noreply.github.com>
2024-04-12 12:56:41 -0700

    fix?

--- public/js/Block.js
+++ public/js/Block.js
@@ -8,8 +8,8 @@ class Block {
   }
   init(x, y, hp, type, team, host) {
     this.raw = {};
-    this.id = Math.random();
     for (const i in Block.args) this[Block.args[i]] = arguments[i];
+    this.id = Math.random();
     this.maxHp = hp;
     if (!(this.c = type !== 'fire' && type !== 'airstrike')) this.sd = setTimeout(() => this.destroy(), type === 'fire' ? 2500 : (type === 'smoke' ? 11000 : 6000));
     if (type === 'halfbarrier' || type === 'spike') this.c = false;
