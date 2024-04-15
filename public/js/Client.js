031fe50b5e9d9bfe58c1d5a1a4f0992a78eea536
cs6413110 <129222222+cs6413110@users.noreply.github.com>
2024-04-15 09:06:17 -0700

    ok fix

--- public/js/Client.js
+++ public/js/Client.js
@@ -396,11 +396,11 @@ class Client {
       } else {
         GUI.draw.fillStyle = '#ffffff';
         GUI.draw.globalAlpha = .25*Math.abs(Math.sin(Math.PI*.5*((((Date.now()-(this.timers[['class', 'powermissle', 'toolkit', 'boost', 'grapple'][i]].time+[this.timers.class.cooldown, 10000, 40000, 5000, 5000][i]))%4000)/1000)-3)));
-        GUI.draw.fillRect([308, 408, 1120, 1196, 1272][i], 952+Math.min((Date.now()-this.timers[['class', 'powermissle', 'toolkit', 'boost', 'grapple'][i]])/[this.timers.class.cooldown, 10000, 40000, 5000, 5000][i], 1)*48, 48, 48);
+        GUI.draw.fillRect([308, 408, 1120, 1196, 1272][i], 952+Math.min((Date.now()-this.timers[['class', 'powermissle', 'toolkit', 'boost', 'grapple'][i]].time)/[this.timers.class.cooldown, 10000, 40000, 5000, 5000][i], 1)*48, 48, 48);
       }
       GUI.draw.fillStyle = PixelTanks.userData.color;
       GUI.draw.globalAlpha = 1;
-      GUI.draw.fillRect([308, 408, 1120, 1196, 1272][i], 952+Math.min((Date.now()-this.timers[['class', 'powermissle', 'toolkit', 'boost', 'grapple'][i]])/[this.timers.class.cooldown, 10000, 40000, 5000, 5000][i], 1)*48, 48, 48);
+      GUI.draw.fillRect([308, 408, 1120, 1196, 1272][i], 952+Math.min((Date.now()-this.timers[['class', 'powermissle', 'toolkit', 'boost', 'grapple'][i]].time)/[this.timers.class.cooldown, 10000, 40000, 5000, 5000][i], 1)*48, 48, 48);
     }
     GUI.drawText(this.dedTime < Date.now()-10000 ? 'Hit F to Respawn' : this.hostupdate?.global || '', 800, 30, 60, '#ffffff', .5);
     GUI.drawText('', 0, 0, 30, '#ffffff', 0); // set font size :) can probably be changed
