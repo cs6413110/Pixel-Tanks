1de31f3274feb4e1b95dca35c8343ed983e72331
cs6413110 <129222222+cs6413110@users.noreply.github.com>
2024-04-15 08:40:52 -0700

    #449

--- public/js/Client.js
+++ public/js/Client.js
@@ -294,7 +294,7 @@ class Client {
   frame() {
     GUI.clear();
     this._fps++;
-    this.render = requestAnimationFrame(this.frame.bind(this));
+    this.render = requestAnimationFrame(() => this.frame());
     if (this.socket) {
       if (this.socket.status === 'connecting') {
         return this.drawStatus('Connecting...');
@@ -389,9 +389,15 @@ class Client {
       GUI.draw.fillRect(c[i], 908+Math.min((Date.now()-this.timers.items[i].time)/this.timers.items[i].cooldown, 1)*92, 92, 92);
     }
     for (let i = 0; i < 4; i++) {
-      if (Date.now()-this.timers[['powermissle', 'toolkit', 'boost', 'grapple'][i]].time > 1) {
+      GUI.draw.fillStyle = PixelTanks.userData.color;
+      if (!this['can'+['Powermissle', 'Toolkit', 'Boost', 'Grapple'][i]]) {
+        GUI.draw.fillStyle = '#000000';
         GUI.draw.globalAlpha = .5;
-        GUI.draw.fillRect([408, 1120, 1196, 1272][i], 956, 48, 48);
+        GUI.draw.fillRect([408, 1120, 1196, 1272][i], 952, 48, 48);
+      } else {
+        GUI.draw.fillStyle = '#ffffff';
+        GUI.draw.globalAlpha = .25*Math.abs(Math.sin(Math.PI*.5*((((Date.now()-(this.timers[['powermissle', 'toolkit', 'boost', 'grapple'][i]].time+[10000, 40000, 5000, 5000][i]))%4000)/1000)-3)));
+        GUI.draw.fillRect([408, 1120, 1196, 1272][i], 952+Math.min((Date.now()-this.timers[['powermissle', 'toolkit', 'boost', 'grapple'][i]])/[10000, 40000, 5000, 5000][i], 1)*48, 48, 48);
       }
       GUI.draw.globalAlpha = 1;
       GUI.draw.fillRect([408, 1120, 1196, 1272][i], 952+Math.min((Date.now()-this.timers[['powermissle', 'toolkit', 'boost', 'grapple'][i]])/[10000, 40000, 5000, 5000][i], 1)*48, 48, 48);
