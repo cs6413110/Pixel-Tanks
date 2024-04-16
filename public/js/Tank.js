b5efb389be84cf2adceaed76ee98e2e86022cfba
cs6413110 <129222222+cs6413110@users.noreply.github.com>
2024-04-16 11:58:12 -0700

    Update Tank.js

--- public/js/Tank.js
+++ public/js/Tank.js
@@ -1,6 +1,6 @@
 class Tank {
   constructor(data, host) {
-    this.id = Engine.getId(0);
+    this.id = Engine.genId(0);
     this.raw = {id: this.id};
     this.host = host;
     this.render = {release: () => {}, b: new Set(), pt: new Set(), ai: new Set(), s: new Set(), d: new Set()};
