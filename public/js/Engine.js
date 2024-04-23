61153bc4e27fc7d57f9716b6d42b680ab29322e0
cs6413110 <129222222+cs6413110@users.noreply.github.com>
2024-04-23 11:59:09 -0700

    ruin bread's career

--- public/js/Engine.js
+++ public/js/Engine.js
@@ -58,6 +58,7 @@ class Engine {
     } else if (a.includes('block#')) {
       const coords = [{ r: [337.5, 360], dx: -10, dy: 80 }, { r: [0, 22.5], dx: -10, dy: 80 }, { r: [22.5, 67.5], dx: -100, dy: 80 }, { r: [67.5, 112.5], dx: -100, dy: -10 }, { r: [112.5, 157.5], dx: -100, dy: -100 }, { r: [157.5, 202.5], dx: -10, dy: -100 }, { r: [202.5, 247.5], dx: 80, dy: -100 }, { r: [247.5, 292.5], dx: 80, dy: -10 }, { r: [292.5, 337.5], dx: 80, dy: 80 }];
       const type = a.replace('block#', '');
+      if (type === 'gold') type = 'strong';
       for (const coord of coords) {
         if (t.r >= coord.r[0] && t.r < coord.r[1]) {
           this.b.push(A.template('Block').init(t.x+coord.dx, t.y+coord.dy, {strong: 200, weak: 100, gold: 300, spike: 100, barrel: 100}[type], type, t.team, this));
