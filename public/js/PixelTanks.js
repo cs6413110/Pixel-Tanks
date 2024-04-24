bf9d7cbcb701e5b1eff211fa020415f2b3cd4634
BreadSpam <143108218+BreadSpam@users.noreply.github.com>
2024-04-24 10:02:37 -0700

    Update PixelTanks.js

--- public/js/PixelTanks.js
+++ public/js/PixelTanks.js
@@ -844,9 +844,6 @@ class PixelTanks {
     for (let i = 0; i < stuffAmount; i++) {
       namber += 2500/stuffAmount;
       setTimeout(() => {
-        clearInterval(render);
-        Menus.trigger('crate');
-        PixelTanks.save();
         const price = type ? 5 : 1, name = type ? 'deathEffects' : 'cosmetics', rand = Math.floor(Math.random()*1001), crate = [{
           common: ['white horns', 'white wings', 'blue horns', 'gold horns', 'blue wings', 'gold wings', 'watermelon', 'Spooked', 'Cute Eyes', 'Army', 'Top Hat', 'X', 'Red Hoodie', 'Devil Wings', 'Devil Horns', 'Exclaimation Point', 'Orange Hoodie', 'GoldShield', 'Yellow Hoodie', 'Green Hoodie', 'Blue Hoodie', 'Purple Hoodie', 'Cancelled', 'Spirals', 'Speaker', 'Spikes', 'Candy Cane', 'Pumpkin Face', 'Mask', 'Purple-Pink Hoodie', 'Bunny Ears'],
           uncommon: ['glitch', 'CompanionCube', 'PortalCube', 'half glitch', 'eye', 'Anime Eyes', 'Angry Eyes', 'Hard Hat', 'Present', 'Dead', 'Peace', 'Question Mark', 'Small Scratch', 'Kill = Ban', 'Reindeer Hat', 'Pumpkin Hat', 'Cat Ears', 'Cake', 'Cat Hat', 'bread', 'First Aid', 'silver', 'Fisher Hat', 'chip', 'eyes', 'zombie', 'googly', 'static', 'lava', 'void knight'],
@@ -905,6 +902,9 @@ class PixelTanks {
           PixelTanks.save();
         }, 2500/stuffAmount);*/
       }, namber);
+      clearInterval(render);
+      Menus.trigger('crate');
+      PixelTanks.save();
     }
     clearInterval(render);
     Menus.trigger('crate');
