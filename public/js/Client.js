b3ae24639abde9f2caca53881cdabae2c9d3a498
BreadSpam <143108218+BreadSpam@users.noreply.github.com>
2024-04-25 12:29:40 -0700

    Update Client.js

--- public/js/Client.js
+++ public/js/Client.js
@@ -765,7 +765,7 @@ class Client {
         this.timers.class = {time: Date.now(), cooldown: 25000};
       } else if (c === 'builder') {
         this.tank.use.push('turret');
-        this.timers.class = {time: Date.now(), cooldown: 20000}
+        this.timers.class = {time: Date.now(), cooldown: 40000}
       } else if (c === 'warrior') {
         this.tank.use.push('bash');
         clearTimeout(this.booster);
