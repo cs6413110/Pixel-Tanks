71bb97aaf9a67f7423b8d1a9e574392275449f7e
cs6413110 <129222222+cs6413110@users.noreply.github.com>
2024-04-30 20:38:53 -0700

    I'm tired

--- public/js/Client.js
+++ public/js/Client.js
@@ -185,7 +185,7 @@ class Client {
   drawBlock(b) {
     GUI.draw.fillStyle = '#ffffff';
     const size = b.type === 'airstrike' ? 200 : 100, type = ['airstrike', 'fire'].includes(b.type) && Engine.getTeam(this.team) === Engine.getTeam(b.team) ? 'friendly'+b.type : b.type;
-    GUI.drawImage(PixelTanks.images.blocks[type], b.x, b.y, size, size, 1, 0, 0, 0, 0, undefined, type === fire ? Math.floor((this.animate%2000)/500)*50 : 0, 0, 50, 50);
+    GUI.drawImage(PixelTanks.images.blocks[type], b.x, b.y, size, size, 1, 0, 0, 0, 0, undefined, type === 'fire' ? Math.floor((this.animate%2000)/500)*50 : 0, 0, 50, 50);
   }
 
   drawShot(s) {
