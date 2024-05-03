f6ee5c089a3864b73cebad661b3022e6077018a5
BreadSpam <143108218+BreadSpam@users.noreply.github.com>
2024-05-03 10:33:13 -0700

    Update Client.js

--- public/js/Client.js
+++ public/js/Client.js
@@ -698,7 +698,7 @@ class Client {
       cooldown = 25000;
     } else if (id === 'flashbang') {
       this.tank.use.push(`flashbang${this.mouse.x+this.tank.x-850}x${this.mouse.y+this.tank.y-550}`);
-      cooldown = 10000;
+      cooldown = 20000;
     } else if (id === 'bomb') {
       this.tank.use.push('bomb');
       this.tank.use.push('break');
