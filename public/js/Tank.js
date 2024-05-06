79a2b0e39848416d4a549e3eb63255bb24caa4cf
cs6413110 <129222222+cs6413110@users.noreply.github.com>
2024-05-06 13:29:34 -0700

    Update Tank.js

--- public/js/Tank.js
+++ public/js/Tank.js
@@ -18,7 +18,7 @@ class Tank {
     this.x = host.spawn.x;
     this.y = host.spawn.y;
     this.logs = this.shields = this.r = this.pushback = this.baseRotation = this.baseFrame = 0;
-    this.update = A.template('msg');
+    this.update = {u: [], d: []};
     this.privateLogs = A.template('arr');
     host.updateEntity(this, this.x, this.y, 80, 80, this.x, this.y, Tank.raw);
     host.override(this);
