0d193048a16cd78303e0c404526a3d23142db305
cs6413110 <129222222+cs6413110@users.noreply.github.com>
2024-05-07 09:23:07 -0700

    bandwidth locking + update postponing #70

--- multiplayer.js
+++ multiplayer.js
@@ -234,8 +234,9 @@ class Multiplayer extends Engine {
         }
       }
       if ((t.msg.logs.length || t.msg.u.length || t.msg.d.length || t.msg.global)) {
-        if (/*(!t.lastSend || (Date.now()-t.lastSend >= 1000/settings.ups)) && */true) {
-          t.socket.send(t.msg);
+        if (/*(!t.lastSend || (Date.now()-t.lastSend >= 1000/settings.ups)) && */!t.busy) {
+          t.busy = true;
+          t.socket.send(t.msg, {}, () => (t.busy = false));
           t.lastSend = Date.now();
           t.msg.u.length = t.msg.d.length = 0;
           t.msg.global = t.msg.logs = undefined;
