c1fbb4b1f8d71a386c721e27af622998d3c8323a
cs6413110 <129222222+cs6413110@users.noreply.github.com>
2024-05-09 07:57:07 -0700

    move lag update over(bread add ur smoke now)

--- public/js/Block.js
+++ public/js/Block.js
@@ -1,37 +1,34 @@
 class Block {
   static args = ['x', 'y', 'hp', 'type', 'team', 'host'];
-  static raw = ['x', 'y', 'maxHp', 'hp', 'type', 's', 'team'];
+  static raw = ['x', 'y', 'type', 'team', 'maxHp', 'hp'];
+  static raw2 = ['x', 'y', 'type', 'team'];
+  static update = ['s', 'hp'];
   constructor() {
     this.cells = new Set();
     this.t = []; // can be removed maybe?
-    for (const p of Block.raw) Object.defineProperty(this, p, {get: () => this.raw[p], set: v => this.setValue(p, v), configurable: true});
   }
   init(x, y, hp, type, team, host) {
-    this.id = Math.random();
+    this.id = host.genId(1);
     this.raw = {id: this.id};
-    for (let i = Block.args.length-1; i >= 0; i--) this[Block.args[i]] = arguments[i];
+    for (const i in Block.args) this[Block.args[i]] = arguments[i];
     this.maxHp = hp;
-    if (!(this.c = type !== 'fire' && type !== 'airstrike' && type !== 'smoke')) this.sd = setTimeout(() => this.destroy(), type === 'fire' ? 2500 : 6000);
-    if (type === 'halfbarrier' || type === 'spike') this.c = false;
+    if (!(this.c = type !== 'fire' && type !== 'airstrike')) this.sd = setTimeout(() => this.destroy(), type === 'fire' ? 2500 : 6000);
+    if (type === 'spike') this.c = false;
     if (type === 'airstrike') for (let i = 0; i < 80; i++) this.t.push(setTimeout(() => this.host.d.push(A.template('Damage').init(this.x+Math.floor(Math.random()*250)-50, this.y+Math.floor(Math.random()*250)-50, 100, 100, 50, this.team, this.host)), 5000+Math.random()*500));
-    if (type === 'smoke') for (let i = 0; i < 800; i++) this.t.push(setTimeout(() => this.host.d.push(A.template('Damage').init(this.x+Math.floor(Math.random()*350)-200, this.y+Math.floor(Math.random()*350)-200, 200, 200, 0, this.team, this.host)), Math.random()*6000));
-    let dxmin = Math.max(0, Math.min(29, Math.floor(this.x/100))), dymin = Math.max(0, Math.min(29, Math.floor(this.y/100))), dxmax = Math.max(0, Math.min(29, Math.floor((this.x+99)/100))), dymax = Math.max(0, Math.min(29, Math.floor((this.y+99)/100)));
-    for (let x = dxmin; x <= dxmax; x++) for (let y = dymin; y <= dymax; y++) {
-      host.cells[x][y].add(this);
-      this.cells.add(x+'x'+y);
-    }
-    if (this.c && this.x % 100 === 0 && this.y % 100 === 0 && this.x >= 0 && this.x <= 2900 && this.y >= 0 && this.y <= 2900) host.map.setWalkableAt(dxmin, dymin, false);
+    if (this.c && this.x % 100 === 0 && this.y % 100 === 0 && this.x >= 0 && this.x <= 2900 && this.y >= 0 && this.y <= 2900) host.map.setWalkableAt(Engine.r(Math.floor(this.x/100)), Engine.r(Math.floor(this.y/100)), false);
+    host.loadCells(this, this.x, this.y, 100, 100);
+    host.updateEntity(this, this.x, this.y, 100, 100, this.x, this.y, type === 'void' || type === 'barrier' ? Block.raw2 : Block.raw);
+    for (const p of Block.raw) this.raw[p] = this[p]; // temp
     return this;
-  }
-  setValue(p, v) {
-    this.updatedLast = Date.now();
-    this.raw[p] = v; // replace soon
-    this.host.updateEntity(this.id, this.x, this.y, 100, 100, p, v); // new bindings
   }
   damage(d, type) {
     if (this.hp === Infinity) return;
     this.s = Date.now();
-    if ((this.hp = Math.min(this.maxHp, this.hp-d)) <= 0) this.destroy();
+    if ((this.hp = Math.min(this.maxHp, this.hp-d)) <= 0) return this.destroy();
+    this.host.updateEntity(this, this.x, this.y, 100, 100, this.x, this.y, Block.update);
+    this.updatedLast = Date.now();
+    this.raw.s = this.s;
+    this.raw.hp = this.hp; // temp
   }
   reset() {
     for (const property of ['x', 'y', 'maxHp', 'hp', 'type', 'team', 's' ,'c', 'updatedLast', 'host']) this[property] = undefined;
