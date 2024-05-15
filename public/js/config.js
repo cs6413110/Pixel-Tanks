3bdedcf74d553f24f08cdaa26db2299d865e6748
@RealDarkMemeGod <145387003+DarkMemeGod411@users.noreply.github.com>
2024-05-15 09:49:51 -0700

    Update config.js

--- public/js/config.js
+++ public/js/config.js
@@ -37,6 +37,7 @@ const images = {
   },
   cosmetics: {
     'Plasma Ball': '/cosmetics/Plasma',
+    'PWR-DMG's-HELM': '/cosmetics/pwr-dmg-helm',
     'venom': '/cosmetics/venom',
     'toxic': '/cosmetics/toxic',
     'spoider': '/cosmetics/spoider',
