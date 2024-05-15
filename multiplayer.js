b09c24a15344a446f98364c56b3a8014733500de
cs6413110 <129222222+cs6413110@users.noreply.github.com>
2024-05-15 10:31:57 -0700

    skrew you life

--- multiplayer.js
+++ multiplayer.js
@@ -143,11 +143,11 @@ class Multiplayer extends Engine {
     for (let oys = (yda > 0 ? -1 : 0)+ocy+h/2*yda, y = m(oys), l = false; (yda < 0 ? (y < m2(oys-h*yda)) : (y > m2(oys-h*yda))); y -= yda) {
       if (yda > 0 ? y <= oys-yl : y >= oys-yl) l = true;
       for (let oxs = (xda > 0 ? -1 : 0)+ocx+w/2*xda, x = m(oxs); (xda < 0 ? (x < m2(oxs-(l ? Math.min(w, Math.abs(xd)) : w)*xda)) : (x > m2(oxs-(l ? Math.min(w, Math.abs(xd)) : w)*xda))); x -= xda) {
-        for (const e of this.cells[x][y]) {
-          if (e.cells && e.cells.find(c => {
+        entity: for (const e of this.cells[x][y]) {
+          for (const c of e.cells) {
             const [a, b] = c.split('x');
-            return xmin <= a && a <= xmax && ymin <= b && b <= ymax;
-          })) continue;
+            if (xmin <= a && a <= xmax && ymin <= b && b <= ymax) continue entity;
+          }
           let i = t.msg.u.findIndex(u => u[0] === e.id);
           if (i !== -1) t.msg.u.splice(i, 1);
           t.msg.d.push(e.id);
