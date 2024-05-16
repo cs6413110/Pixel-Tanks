553d119247e351ded0b937e7154c6b89110bb667
BreadSpam <143108218+BreadSpam@users.noreply.github.com>
2024-05-16 08:11:41 -0700

    Update Client.js

--- public/js/Client.js
+++ public/js/Client.js
@@ -439,6 +439,7 @@ class Client {
     for (let i = 0; i < 4; i++) {
       const item = PixelTanks.userData.items[i];
       GUI.drawImage(PixelTanks.images.items[item], c[i], 908, 92, 92, 1);
+      GUI.drawImage(PixelTanks.images.items[PixelTanks.userData.class+'ui'], 308, 952, 48, 48, 1);
       if (!this['canItem'+i]) {
         GUI.draw.fillStyle = '#000000';
         GUI.draw.globalAlpha = .5;
