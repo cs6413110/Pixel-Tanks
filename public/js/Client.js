f555d21bc017b8409e73ecc5d39de28229f3ca84
BreadSpam <143108218+BreadSpam@users.noreply.github.com>
2024-05-22 08:05:36 -0700

    Update Client.js

--- public/js/Client.js
+++ public/js/Client.js
@@ -320,7 +320,7 @@ class Client {
     } else if (teamname === 'BLUE') {
       GUI.drawText(username, t.x+40, t.y-25, 50, '#0000ff', 0.5);
     } else if (teamname === 'LOBBY') {
-      if (t.color === '#ff0000') {
+      if (t.color === '#FF0000') {
         GUI.drawText(username, t.x+40, t.y-25, 50, '#ff0000', 0.5);
       } else GUI.drawText(username, t.x+40, t.y-25, 50, '#0000ff', 0.5);
     } else GUI.drawText(username, t.x+40, t.y-25, 50, '#ffffff', 0.5);
