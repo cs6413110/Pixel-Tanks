e25976a127f9aec8a59cff5340dd925b4daf6ec6
cs6413110 <129222222+cs6413110@users.noreply.github.com>
2024-07-25 14:00:19 -0700

    Spam prevention + Mute requirement #598

--- multiplayer.js
+++ multiplayer.js
@@ -605,6 +605,9 @@ const Commands = {
     t.socket.send({status: 'error', message: s});
   }],
   requestunmute: [Object, 4, 1, function(data) {
+    if (!Storage.mutes.includes(this.username)) return this.send({status: 'error', message: `You aren't muted!`});
+    if (s.muteTimer && s.muteTimer+10000 > Date.now()) return this.send({status: 'error', message: `Wait 10 seconds before using this!`});
+    s.muteTimer = Date.now();
     for (const s of Object.values(servers)) for (const t of s.pt) if (Storage.admins.includes(t.username) || Storage.owners.includes(t.username)) t.privateLogs.push({m: this.username+' requested to be unmuted!', c: '#ffff00'});
   }],
   msg: [Object, 4, -1, function(data) {
