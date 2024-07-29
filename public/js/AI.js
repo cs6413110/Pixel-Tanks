30baf47e8bcb9c48ea8a182350ac4d3024e24619
BreadSpam <143108218+BreadSpam@users.noreply.github.com>
2024-07-29 10:29:33 -0700

    this will be funny lol

--- public/js/AI.js
+++ public/js/AI.js
@@ -72,6 +72,7 @@ class AI {
       if (this.canPowermissle && this.role !== 0 && Math.random() <= 1/600) this.fireCalc(this.target.x, this.target.y, 'powermissle');
       if (this.canFire) this.fireCalc(this.target.x, this.target.y);
     }
+    if (this.role === 4 && Math.sqrt((this.target.x - this.x) ** 2 + (this.target.y - this.y) ** 2) < 50) this.host.useAbility(this, 'airstrike'+this.target.x+'x'+this.target.y);
     if (this.canClass && this.mode !== 0 && Math.random() < 1/300) {
       let cooldown = 0;
       if (this.class === 'tactical') {
