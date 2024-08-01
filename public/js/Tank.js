c80834804c30d6f0260666093e4a942274888cd9
cs6413110 <129222222+cs6413110@users.noreply.github.com>
2024-08-01 09:54:20 -0700

    a

--- public/js/Tank.js
+++ public/js/Tank.js
@@ -52,8 +52,8 @@ class Tank {
       }
       for (const e of enemies) this.eradar.push(Engine.toAngle(e.x-this.x, e.y-this.y))
       for (const f of frens) this.fradar.push(Engine.toAngle(f.x-this.x, f.y-this.y));
-      if (enemies.length) this.host.updateEntity(this, ['eradar']);
-      if (frens.length) this.host.updateEntity(this, ['fradar']);
+      this.host.updateEntity(this, ['eradar']);
+      this.host.updateEntity(this, ['fradar']);
     }
     if (this.dedEffect) {
       this.dedEffect.time = Date.now() - this.dedEffect.start;
