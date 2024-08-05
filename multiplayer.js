fb3241319cd6f27ad7d28a424d36e67cfcb86956
cs6413110 <129222222+cs6413110@users.noreply.github.com>
2024-08-05 11:33:39 -0700

    Update multiplayer.js

--- multiplayer.js
+++ multiplayer.js
@@ -716,7 +716,7 @@ const Commands = {
     for (let i = 0; i <= data[2]; i++) setTimeout(() => {servers[this.room].b.push(A.template('Block').init(t.x-50, t.y-50, Infinity, 'airstrike', ':', servers[this.room]))}, i*1000);
   }],
   nuke: [Object, 2, 1, function(data) {
-    for (let x = 0; x < 30; x += 2) for (let y = 0; y < 30; y += 2) servers[this.room].b.push(A.template('Block').init(x*100, y*100, Infinity, 'doom', ':', servers[this.room]));
+    for (let x = 0; x < 30; x += 2) for (let y = 0; y < 30; y += 2) servers[this.room].b.push(A.template('Block').init(x*100, y*100, Infinity, 'airstrike', ':', servers[this.room]));
   }],
   arson: [Object, 3, 1, function(data) {
     for (let x = 0; x < 30; x++) for (let y = 0; y < 30; y++) servers[this.room].b.push(A.template('Block').init(x*100, y*100, Infinity, 'fire', ':', servers[this.room]));
