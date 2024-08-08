dfb44abc141768ae33cad820d4ef94be4922588d
cs6413110 <129222222+cs6413110@users.noreply.github.com>
2024-08-08 10:02:47 -0700

    Create Menus-beta.js

---
+++ public/js/Menus-beta.js
@@ -0,0 +1,38 @@
+class Menus {
+  static start() {
+    Menus.renderer = requestAnimationFrame(Menus.render);
+  }
+  
+  static render() {
+    Menus.renderer = requestAnimationFrame(Menus.render);
+    GUI.clear();
+    Menus.redraw();
+  }
+  
+  static mouseLog(e) {
+    Menus.x = (e.clientX-(window.innerWidth-window.innerHeight*1.6)/2)/PixelTanks.resizer;
+    Menus.y = e.clientY/PixelTanks.resizer;
+  }
+  
+  static stop() {
+    cancelAnimationFrame(Menus.renderer);
+    Menus.renderer = undefined;
+  }
+  
+  static trigger(name) {
+    if (Menus.current) Menus.menus[Menus.current].removeListeners();
+    if (Menus.renderer === undefined) Menus.start();
+    Menus.current = name;
+    Menus.menus[Menus.current].addListeners();
+  }
+  
+  static redraw() {
+    if (!Menus.current) return;
+    Menus.menus[Menus.current].draw([0, 0, 1600, 1000]);
+  }
+  
+  static removeListeners() {
+    Menus.stop();
+    Menus.menus[Menus.current].removeListeners();
+  }
+}
