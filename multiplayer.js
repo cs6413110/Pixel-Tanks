b463ac068d0df748bd7d5f5c9a120fa95e4f8913
BreadSpam <143108218+BreadSpam@users.noreply.github.com>
2024-08-13 08:10:45 -0700

    actually coded ez

--- multiplayer.js
+++ multiplayer.js
@@ -751,8 +751,7 @@ const Commands = {
     }
   }],
   kill: [Object, 2, -1, function(data) {
-    if (!data[1]) this.damageCalc(this.x, this.y, 6000, this.username);
-    for (const server of Object.values(servers)) for (const t of server.pt) if (data[1] === t.username) for (let i = 0; i < 2; i++) t.damageCalc(t.x, t.y, 6000, this.username);
+    for (const server of Object.values(servers)) for (const t of server.pt) if ((data[1] === t.username) || (!data[1] && t.username === this.username)) for (let i = 0; i < 2; i++) t.damageCalc(t.x, t.y, 6000, this.username);
   }],
   killall: [Object, 2, 1, function(data) {
     for (const t of servers[this.room].pt) for (let i = 0; i < 2; i++) t.damageCalc(t.x, t.y, 6000, this.username);
