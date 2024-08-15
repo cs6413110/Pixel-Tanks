d178f83b96beaaa6d803401194944cf430d33e73
cs6413110 <129222222+cs6413110@users.noreply.github.com>
2024-08-15 10:26:20 -0700

    ez

--- public/js/Menu-beta.js
+++ public/js/Menu-beta.js
@@ -1,16 +1,16 @@
 class Menu {
   constructor(data, id) {
     this.id = id;
-    this.buttons = data.buttons;
     this.listeners = data.listeners;
     this.cdraw = data.cdraw.bind(this);
-    this.listeners.click = this.onclick;
-    for (const l in this.listeners) this.listeners[l] = this.listeners[l].bind(this);
-    for (const b of this.buttons) {
-      if (typeof b[4] === 'function') b[4] = b[4].bind(this);
-      b[6] = 0;
+    for (const b of data.buttons) {
+      let button = document.createElement('button');
+      button.style = 'position: abosolute;';
+      button.onclick = () => (typeof b[4] === 'function' ? b[4]() :  Menus.trigger(b[4]));
+      button.innerText = id+'.'+b[4];
+      document.body.appendChild(button);
     }
-    this.render = [0, 0, 1600, 1000];
+    this.render = [0, 0, 1600, 1000]; // rewrite structure
     if (PixelTanks.images.menus[this.id] !== undefined) {
       const oldload = PixelTanks.images.menus[this.id].onload;
       PixelTanks.images.menus[this.id].onload = () => {
@@ -21,35 +21,18 @@ class Menu {
   }
   
   addListeners() {
-    for (const l in this.listeners) window.addEventListener(l, this.listeners[l]);
+    for (const l in this.listeners) window.addEventListener(l, () => this.listeners[l]());
   }
   
   removeListeners() {
     for (const l in this.listeners) window.removeEventListener(l, this.listeners[l]);
   }
-
-  compile() {
-    this.cache = [];
-    for (const b of this.buttons) {
-      const x = this.render[0]+b[0]*this.render[2]/1600, y = this.render[1]+b[1]*this.render[3]/1000, w = b[2]*this.render[2]/1600, h = b[3]*this.render[3]/1000;
-      const canvas = document.createElement('canvas'), draw = canvas.getContext('2d');
-      canvas.width = w*PixelTanks.resizer;
-      canvas.height = h*PixelTanks.resizer;
-      draw.setTransform(PixelTanks.resizer, 0, 0, PixelTanks.resizer, -x*PixelTanks.resizer, -y*PixelTanks.resizer);
-      if (PixelTanks.images.menus[this.id]) draw.drawImage(PixelTanks.images.menus[this.id], this.render[0], this.render[1], this.render[2], this.render[3]);
-      this.cache.push([x, y, w, h, canvas]);
-    }
-
-    this.b = [];
-    for (const b of this.buttons) {
-    }
-  }
   
   draw(render) {
     if (render && JSON.stringify(render) !== JSON.stringify(this.render)) {
       this.render = render;
-      this.compile();
-    }
+      this.compile(); // recompile???
+    } // resize change???
     if (PixelTanks.images.menus[this.id]) GUI.drawImage(PixelTanks.images.menus[this.id], this.render[0], this.render[1], this.render[2], this.render[3], 1);
     this.cdraw();
   }
