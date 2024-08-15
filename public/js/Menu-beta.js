47e9b66a361dd71a30d516bf3534880e9d0a4f23
cs6413110 <129222222+cs6413110@users.noreply.github.com>
2024-08-15 11:07:00 -0700

    rttm

--- public/js/Menu-beta.js
+++ public/js/Menu-beta.js
@@ -5,22 +5,21 @@ class Menu {
     this.id = id;
     this.listeners = data.listeners;
     this.cdraw = data.cdraw.bind(this);
-    for (const b of data.buttons) {
-      let button = document.createElement('INPUT');
-      button.type = 'image';
-      button.onclick = () => (typeof b[4] === 'function' ? b[4]() :  Menus.trigger(b[4]));
-      button.width = Menu.scaler.width = b[2];
-      button.height = Menu.scaler.height = b[3];
-      Menu.scaler.getContext('2d').drawImage(PixelTanks.images.menus[id], -b[0], -b[1]);
-      button.src = Menu.scaler.toDataURL();
-      document.body.appendChild(button);
-    }
     this.render = [0, 0, 1600, 1000]; // rewrite structure
     if (PixelTanks.images.menus[this.id] !== undefined) {
       const oldload = PixelTanks.images.menus[this.id].onload;
       PixelTanks.images.menus[this.id].onload = () => {
         oldload();
-        this.compile();
+        for (const b of data.buttons) {
+          let button = document.createElement('INPUT');
+          button.type = 'image';
+          button.onclick = () => (typeof b[4] === 'function' ? b[4]() :  Menus.trigger(b[4]));
+          button.width = Menu.scaler.width = b[2];
+          button.height = Menu.scaler.height = b[3];
+          Menu.scaler.getContext('2d').drawImage(PixelTanks.images.menus[id], -b[0], -b[1]);
+          button.src = Menu.scaler.toDataURL();
+          document.body.appendChild(button);
+        }
       }
     }
     } catch(e) {
