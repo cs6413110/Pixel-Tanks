e538b8fe8365cb42fedacac74f718730d78ebe35
cs6413110 <129222222+cs6413110@users.noreply.github.com>
2024-08-15 11:30:02 -0700

    cant spell

--- public/js/Menu-beta.js
+++ public/js/Menu-beta.js
@@ -16,7 +16,7 @@ class Menu {
           button.type = 'image';
           button.onclick = () => (typeof b[4] === 'function' ? b[4]() :  Menus.trigger(b[4]));
           button.width = 1000*(Menu.scaler.width = b[2])/window.innerHeight;
-          button.height = 1000*(Menu.scaler.height = b[3])/winow.innerHeight;
+          button.height = 1000*(Menu.scaler.height = b[3])/window.innerHeight;
           button.style = 'position: absolute; left: calc((100vw-100vh*1.6)/2+'+b[0]+'px); top: '+b[1]+'px';
           Menu.scaler.getContext('2d').drawImage(PixelTanks.images.menus[id], -b[0], -b[1]);
           button.src = Menu.scaler.toDataURL();
