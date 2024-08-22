56e2a50f435ddcacb7398ff06beacb7d01d1b6bf
cs6413110 <129222222+cs6413110@users.noreply.github.com>
2024-08-22 09:04:24 -0700

    sizing

--- public/js/Menu-beta.js
+++ public/js/Menu-beta.js
@@ -14,13 +14,12 @@ class Menu {
     this.elements = this.elements.reduce((a, c) => (c.tagName !== 'button' ? a.concat(c) : a), []);
     for (const b of this.buttons) {
       let button = document.createElement('BUTTON'), image = document.createElement('IMG');
-      button.onclick = () => (typeof b[4] === 'function' ? b[4].bind(this)() :  Menus.trigger(b[4]));
-      button.width = window.innerHeight*(button.w = Menu.scaler.width = b[2])/1000;
-      button.height = window.innerHeight*(button.h = Menu.scaler.height = b[3])/1000;
+      button.onclick = () => (typeof b[4] === 'function' ? b[4].bind(this)() : Menus.trigger(b[4]));
+      button.width = image.width = window.innerHeight*(button.w = Menu.scaler.width = b[2])/1000;
+      button.height = image.height = window.innerHeight*(button.h = Menu.scaler.height = b[3])/1000;
       const leftOffset = (window.innerWidth-window.innerHeight*1.6)/2+(button.x = b[0])/1000*window.innerHeight;
       Menu.scaler.getContext('2d').drawImage(GUI.canvas, -b[0], -b[1], 1600, 1000);
       image.src = Menu.scaler.toDataURL();
-      image.style = 'max-width: 100%; max-height: 100%';
       button.style = 'position: absolute; left: '+leftOffset+'px; top: '+(window.innerHeight*(button.y = b[1])/1000)+'px; padding: 0; border: 0;';
       button.appendChild(image);
       document.body.appendChild(button);
@@ -31,6 +30,10 @@ class Menu {
     for (const e of this.elements) {
       e.width = window.innerHeight*e.w/1000;
       e.height = window.innerHeight*e.h/1000;
+      if (e.children.length) for (const c of e.children) {
+        c.width = window.innerHeight*e.w/1000;
+        c.height = window.innerHeight*e.h/1000;
+      }
       e.style.top = (window.innerHeight*e.x/1000)+'px';
       e.style.left = (window.innerHeight*e.y/1000)+'px';
     }
