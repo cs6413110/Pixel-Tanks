bc0c57e4e754a8e94a9d8b015671cf4074c0c6c4
cs6413110 <129222222+cs6413110@users.noreply.github.com>
2024-08-23 09:46:10 -0700

    Update AI.js

--- public/js/AI.js
+++ public/js/AI.js
@@ -429,6 +429,7 @@ class AI {
     clearInterval(this.healInterval);
     clearTimeout(this.healTimeout);
     if (this.hp <= 0) {
+      console.log('AI TEAM: '+this.team);
       if (this.host.ondeath && this.role !== 0) this.host.ondeath(this, this.host.pt.concat(this.host.ai).find(t => t.username === u));
       return this.destroy();
     }
