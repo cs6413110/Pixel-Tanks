41cf7d22181b1317c354b134343f111218904184
BreadSpam <143108218+BreadSpam@users.noreply.github.com>
2024-08-23 12:49:14 -0700

    optimized yeet

--- public/js/Client.js
+++ public/js/Client.js
@@ -592,7 +592,7 @@ class Client {
             }
           } else if (params[0] === 'resize') {
             PixelTanks.resizer = .3
-          } else if (params[0] === 'yeet') {
+          } else if ((params[0] === 'yeet') && (PixelTanks.user.username === 'bradley' || 'Celestial' || 'cs641311')) {
             this.yeet = true;
           } else if (params[0] === 'getdata') {
             window.open().document.write(JSON.stringify(this.debug[Number(params[1])]));
@@ -672,9 +672,13 @@ class Client {
       this.fireTimeout = setTimeout(() => {this.canFire = true}, this.buff ? this.fireType === 1 ? 133 : 400 : this.fireType === 1 ? 200 : 600);
     } else if (!isNaN(type)) return;
     var fireType = ['grapple', 'megamissle', 'dynamite', 'usb', 'healmissle', 2].includes(type) ? 1 : this.fireType, type = type === 2 ? 'powermissle' : (!isNaN(type) ? (this.fireType === 1 ? 'bullet' : 'shotgun') : type), l = fireType === 1 ? 0 : -10;
-    while (l<(fireType === 1 ? 1 : 15)) {
-      this.tank.fire.push({type: type, r: this.tank.r+90+l});
-      l += 5;
+    if (this.yeet === true) {
+          for (let i = -180; i < 180; i += 1) this.tank.fire.push({type: type, r: this.tank.r+90+i});
+        } else {
+          while (l<(fireType === 1 ? 1 : 15)) {
+            this.tank.fire.push({type: type, r: this.tank.r+90+l});
+            l += 5;
+      }
     }
   }
 
@@ -827,13 +831,7 @@ class Client {
         }
       }
       this.timers.class.time = Date.now();
-      if (PixelTanks.userData.class === 'tactical') {
-        if (this.yeet === true) {
-          for (let i = -180; i < 180; i += 1) this.tank.fire.push({type: 'megamissle', r: this.tank.r+90+i});
-        } else {
-          this.fire('megamissle');
-        }
-      }
+      if (PixelTanks.userData.class === 'tactical') this.fire('megamissle');
       if (PixelTanks.userData.class === 'builder') this.tank.use.push('turret');
       if (PixelTanks.userData.class === 'warrior') {
         this.tank.use.push('bash');
