0daf2bc7b5eb8ec46118abe61a7e6cdc721ff16f
cs6413110 <129222222+cs6413110@users.noreply.github.com>
2024-08-26 07:00:11 -0700

    fix?

--- public/js/Client.js
+++ public/js/Client.js
@@ -373,7 +373,7 @@ class Client {
     if (this.dx) {
       var x = this.dx.o+Math.floor((Date.now()-this.dx.t)/15)*this.dx.a*this.speed*(this.halfSpeed ? .5 : (this.buffed ? 1.5 : 1));
       let xR = this.collision(x, this.tank.y, 'x', this.dx.a), xD = this.collision(this.dx.o, this.tank.y);
-      if (!xD || (xD && !this.collision(x, this.tank.y))) this.tank.x = xR;
+      if (xD || (!xD && this.collision(x, this.tank.y))) this.tank.x = xR;
       this.left = x === xR ? this.dx.a < 0 : null;
       this.dx.t = Date.now()-(Date.now()-this.dx.t)%15;
       this.dx.o = this.tank.x;
@@ -381,7 +381,7 @@ class Client {
     if (this.dy) {
       var y = this.dy.o+Math.floor((Date.now()-this.dy.t)/15)*this.dy.a*this.speed*(this.halfSpeed ? .5 : (this.buffed ? 1.5 : 1));
       let yR = this.collision(this.tank.x, y, 'y', this.dy.a), yD = this.collision(this.tank.x, this.dy.o);
-      if (!yD || (yD && !this.collision(this.tank.x, y))) this.tank.y = yR;
+      if (yD || (!yD && this.collision(this.tank.x, y))) this.tank.y = yR;
       this.up = y === yR ? this.dy.a < 0 : null;
       this.dy.t = Date.now()-(Date.now()-this.dy.t)%15;
       this.dy.o = this.tank.y;
