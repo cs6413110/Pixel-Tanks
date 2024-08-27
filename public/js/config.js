434808823ea3acad3fcf68b2515320f2369b5def
LostKing <jon441491@gmail.com>
2024-08-27 07:10:06 -0700

    added cool crown

--- public/js/config.js
+++ public/js/config.js
@@ -47,7 +47,7 @@ const sourceMap = {
     uncommon: ['chip', 'deep_scratch', 'evil_eye', 'inferno_eye', 'motherboard', 'blue_wings', 'blue_horns', 'white_wings', 'white_horns', 'gold_wings', 'gold_horns', 'devil_wings', 'devil_horns', 'hazard', 'angel_wings', 'bat_wings', 'locked', 'mini_tank', 'dust', 'pouch'],
     rare: ['blue_tint', 'glitch', 'blue_helmet', 'white_helmet', 'helmet', 'gold_helmet', 'hacker_hoodie', 'hoodie', 'purple', 'sus', 'veins', 'cookie'],
     epic: ['christmas_hat', 'christmas_lights', 'dizzy', 'rage', 'toxic', 'crown', 'error', 'dark_crown'],
-    legendary: ['plasma', 'cry',],    //imma just delete the cosmetic
+    legendary: ['plasma', 'cry', 'LostCrown',],    //imma just delete the cosmetic
     admin: [], // LOL IT NEEDS ADMIN TO NOT EPLODE DELETE IT THEN
     mythic: ['terminator', 'mlg_glasses', 'power_armor', 'corrupted'], //STOAP
   },
