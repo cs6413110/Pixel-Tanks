910d84ca9a063e2242330ed9d6a9a5f75bddafba
BreadSpam <143108218+BreadSpam@users.noreply.github.com>
2024-08-28 08:39:24 -0700

    Update Client.js

--- public/js/Client.js
+++ public/js/Client.js
@@ -207,9 +207,9 @@ class Client {
     } else if (s.type === 'shotgun') {
       GUI.drawImage(PixelTanks.images.bullets.shotgun, s.x, s.y, 10, 10, 1, 5, 5, 0, 0, s.r+90);
     } else if (s.type === 'grapple') {
-      let l = Engine.getUsername(s.team) === 'LostKing', m = l ? (Date.now()-this.animate)/1000*360*2 : 0;
+      let l = Engine.getUsername(s.team) === 'LostKing', m = (Date.now()-this.animate)/1000*360*2;
       if (l) return GUI.drawImage(PixelTanks.images.bullets.scythe, s.x-45, s.y-45, 90, 90, 1, 45, 45, 0, 0, s.r+90-m);
-      let l = Engine.getUsername(s.team) === 'bradley', m = l ? (Date.now()-this.animate)/1000*360*2 : 0;
+      l = Engine.getUsername(s.team) === 'bradley';
       if (l) return GUI.drawImage(PixelTanks.images.bullets.yoink, s.x-45, s.y-45, 90, 90, 1, 45, 45, 0, 0, s.r+90-m);
       GUI.drawImage(PixelTanks.images.bullets.grapple, s.x-22.5, s.y-22.5, 45, 45, 1, 22.5, 22.5, 0, 0, s.r+90);
       GUI.draw.lineWidth = 10;
