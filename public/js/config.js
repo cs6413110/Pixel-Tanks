408960cfe538dc10b69c629146dd5c04a32dea5b
BreadSpam <143108218+BreadSpam@users.noreply.github.com>
2024-08-30 08:38:52 -0700

    tapppsss

--- public/js/config.js
+++ public/js/config.js
@@ -10,7 +10,7 @@ const sourceMap = {
     {
       ref: 'bullets',
       path: 'bullets',
-      load: ['normal', 'shotgun', 'powermissle', 'healmissle', 'megamissle', 'grapple', 'dynamite', 'fire', 'usb', 'scythe', 'yoink', 'leech', 'rust'],//DONT REMOVE. STOP BEING MEAN TO DMG :((. idc if its bad, let him have his fun, orelse all customs go away
+      load: ['normal', 'shotgun', 'powermissle', 'healmissle', 'megamissle', 'grapple', 'dynamite', 'fire', 'usb', 'scythe', 'yoink', 'leech', 'rust'],//DONT REMOVE. STOP BEING MEAN TO DMG :((. idc if its bad, let him have his fun, orelse all customs go away. also u broke game by keeping the comma stoopid idot
     },
     {
       ref: 'tanks',
