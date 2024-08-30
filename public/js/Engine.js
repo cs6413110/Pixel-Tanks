569c956086d7eeb8d3afdab471329eb86461b4b1
cs6413110 <129222222+cs6413110@users.noreply.github.com>
2024-08-30 09:01:45 -0700

    Update Engine.js

--- public/js/Engine.js
+++ public/js/Engine.js
@@ -231,7 +231,7 @@ class Engine {
           this.spawns[1] = {x, y};
         } else if (['T', 'W', 'P', 'D'].includes(a[i])) {
           A.template('AI').init(x+10, y+10, ['T', 'W', 'P', 'D'].indexOf(a[i]), 20, 'squad', this);
-        } else if (key[a[i]]) this.b.push(A.template('Block').init(x, y, key[a[i]], 100, ':', this));
+        } else if (key[a[i]]) this.b.push(A.template('Block').init(x, y, key[a[i]], ':', this));
         l++;
       }
     }
