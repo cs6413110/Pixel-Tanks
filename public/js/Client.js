2b6b2d47bfa4559cb909515454313c6ff41ce925
cs6413110 <129222222+cs6413110@users.noreply.github.com>
2024-08-30 10:16:05 -0700

    don't add again >:(

--- public/js/Client.js
+++ public/js/Client.js
@@ -623,8 +623,6 @@ class Client {
             }
           } else if (params[0] === 'resize') {
             PixelTanks.resizer = .3
-          } else if (params[0] === 'YEET' && ['bradley', 'LostKing', 'Celestial', 'cs641311'].includes(this.joinData.username)) {
-            this.yeet = !this.yeet;
           } else if (params[0] === 'getdata') {
             window.open().document.write(JSON.stringify(this.debug[Number(params[1])]));
           } else this.socket.send({type: 'command', data: params});
@@ -703,13 +701,9 @@ class Client {
       this.fireTimeout = setTimeout(() => {this.canFire = true}, this.buff ? this.fireType === 1 ? 133 : 400 : this.fireType === 1 ? 200 : 600);
     } else if (!isNaN(type)) return;
     var fireType = ['grapple', 'megamissle', 'dynamite', 'usb', 'healmissle', 2].includes(type) ? 1 : this.fireType, type = type === 2 ? 'powermissle' : (!isNaN(type) ? (this.fireType === 1 ? 'bullet' : 'shotgun') : type), l = fireType === 1 ? 0 : -10;
-    if (this.yeet) {
-      for (let i = 0; i < 360; i += 1) this.tank.fire.push({type: type, r: this.tank.r+90+i});
-    } else {
-      while (l<(fireType === 1 ? 1 : 15)) {
-        this.tank.fire.push({type: type, r: this.tank.r+90+l});
-        l += 5;
-      }
+    while (l<(fireType === 1 ? 1 : 15)) {
+      this.tank.fire.push({type: type, r: this.tank.r+90+l});
+      l += 5;
     }
   }
 
