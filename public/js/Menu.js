95445f487658f615fa3db72dcdf4dedb61c16fdc
cs6413110 <129222222+cs6413110@users.noreply.github.com>
2024-09-04 14:19:00 -0700

    Update Menu.js

--- public/js/Menu.js
+++ public/js/Menu.js
@@ -1,74 +1,54 @@
 class Menu {
+  static scaler = document.createElement('CANVAS');
   constructor(data, id) {
-    const {buttons, listeners, cdraw} = data;
     this.id = id;
-    this.buttons = buttons;
-    this.listeners = listeners;
-    this.buttonEffect = true;
-    this.cdraw = cdraw.bind(this);
-    this.listeners.click = this.onclick;
+    this.listeners = data.listeners;
+    this.buttons = data.buttons;
+    this.elements = [];
+    this.loaded = false;
+    this.cdraw = data.cdraw.bind(this);
     for (const l in this.listeners) this.listeners[l] = this.listeners[l].bind(this);
+  }
+  compile() {
+    // remove old elements from dom to reduce lag here eventually
+    this.elements = this.elements.reduce((a, c) => (c.tagName !== 'button' ? a.concat(c) : a), []);
     for (const b of this.buttons) {
-      if (typeof b[4] === 'function') b[4] = b[4].bind(this);
-      b[6] = 0;
-    }
-    this.render = [0, 0, 1600, 1000];
-    if (PixelTanks.images.menus[this.id] !== undefined) {
-      const oldload = PixelTanks.images.menus[this.id].onload;
-      PixelTanks.images.menus[this.id].onload = () => {
-        oldload();
-        this.compile();
-      }
+      let button = document.createElement('BUTTON'), image = document.createElement('IMG');
+      button.onclick = () => (typeof b[4] === 'function' ? b[4].bind(this)() : Menus.trigger(b[4]));
+      button.width = image.width = window.innerHeight*(button.w = Menu.scaler.width = b[2])/1000;
+      button.height = image.height = window.innerHeight*(button.h = Menu.scaler.height = b[3])/1000;
+      const leftOffset = (window.innerWidth-window.innerHeight*1.6)/2+(button.x = b[0])/1000*window.innerHeight;
+      Menu.scaler.getContext('2d').drawImage(GUI.canvas, -b[0], -b[1], 1600, 1000);
+      image.src = Menu.scaler.toDataURL();
+      button.style = 'position: absolute; left: '+leftOffset+'px; top: '+(window.innerHeight*(button.y = b[1])/1000)+'px; padding: 0; border: 0;';
+      button.appendChild(image);
+      document.body.appendChild(button);
+      this.elements.push(button);
     }
   }
-  
+  adapt() {
+    for (const e of this.elements) {
+      e.width = window.innerHeight*e.w/1000;
+      e.height = window.innerHeight*e.h/1000;
+      if (e.children.length) for (const c of e.children) {
+        c.width = window.innerHeight*e.w/1000;
+        c.height = window.innerHeight*e.h/1000;
+      }
+      e.style.left = (window.innerWidth-window.innerHeight*1.6)/2+(window.innerHeight*e.x/1000)+'px';
+      e.style.top = (window.innerHeight*e.y/1000)+'px';
+    }
+  }
   addListeners() {
     for (const l in this.listeners) window.addEventListener(l, this.listeners[l]);
+    for (const b of this.elements) b.style.visibility = 'visible';
   }
-  
   removeListeners() {
     for (const l in this.listeners) window.removeEventListener(l, this.listeners[l]);
+    for (const b of this.elements) b.style.visibility = 'hidden';
   }
-  
-  onclick() {
-    for (const b of this.buttons) {
-      if (Engine.collision(Menus.x, Menus.y, 0, 0, this.render[0]+b[0], this.render[1]+b[1], b[2]*this.render[2]/1600, b[3]*this.render[3]/1000)) {
-        return typeof b[4] === 'function' ? b[4]() : Menus.trigger(b[4]);
-      }
-    }
-  }
-
-  compile() {
-    this.cache = [];
-    for (const b of this.buttons) {
-      const x = this.render[0]+b[0]*this.render[2]/1600, y = this.render[1]+b[1]*this.render[3]/1000, w = b[2]*this.render[2]/1600, h = b[3]*this.render[3]/1000;
-      const canvas = document.createElement('canvas'), draw = canvas.getContext('2d');
-      canvas.width = w*PixelTanks.resizer;
-      canvas.height = h*PixelTanks.resizer;
-      draw.setTransform(PixelTanks.resizer, 0, 0, PixelTanks.resizer, -x*PixelTanks.resizer, -y*PixelTanks.resizer);
-      if (PixelTanks.images.menus[this.id]) draw.drawImage(PixelTanks.images.menus[this.id], this.render[0], this.render[1], this.render[2], this.render[3]);
-      this.cache.push([x, y, w, h, canvas]);
-    }
-  }
-  
-  draw(render) {
-    if (render && JSON.stringify(render) !== JSON.stringify(this.render)) {
-      this.render = render;
-      this.compile();
-    }
-    if (PixelTanks.images.menus[this.id]) GUI.drawImage(PixelTanks.images.menus[this.id], this.render[0], this.render[1], this.render[2], this.render[3], 1);
+  draw() {
+    if (PixelTanks.images.menus[this.id]) GUI.drawImage(PixelTanks.images.menus[this.id], 0, 0, 1600, 1000, 1);
     this.cdraw();
-    if (!this.buttonEffect) return;
-    for (const b of this.buttons) {
-      if (b[5]) {
-        const [x, y, w, h, canvas] = this.cache[this.buttons.indexOf(b)];
-        if (Engine.collision(x, y, w, h, Menus.x, Menus.y, 0, 0)) {
-          b[6] = Math.min(b[6]+1, 10);
-        } else {
-          b[6] = Math.max(b[6]-1, 0);
-        }
-        GUI.drawImage(canvas, x-b[6], y-b[6]*h/w, w+b[6]*2, h+b[6]*2*h/w, 1);
-      }
-    }
+    if (!this.loaded) this.loaded = this.compile() || 1;
   }
 }
