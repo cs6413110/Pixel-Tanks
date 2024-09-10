aafb85ed9e1e5cb9562d63e110dbcc8db5f2d16d
cs6413110 <129222222+cs6413110@users.noreply.github.com>
2024-09-10 09:01:18 -0700

    scaling

--- public/js/Client.js
+++ public/js/Client.js
@@ -19,14 +19,14 @@ class Client {
       padding: 2px;
       padding-left: 5px;
       padding-right: 5px;
-      font-size: 20px;
+      font-size: 0.8vh;
       background-color: rgba(0, 0, 0, .2);
     }
     #message img {
       max-width: 200px;
     }
     #input {
-      font-size: 20px;
+      font-size: 0.8vh;
       background-color: rgba(0, 0, 0, .2);
       border: none;
       outline: none;
