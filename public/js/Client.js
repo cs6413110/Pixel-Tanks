375f93fc67cc85cf3e82968a2d1b0082ddd63c94
cs6413110 <129222222+cs6413110@users.noreply.github.com>
2024-09-10 09:08:05 -0700

    Update Client.js

--- public/js/Client.js
+++ public/js/Client.js
@@ -19,14 +19,14 @@ class Client {
       padding: 2px;
       padding-left: 5px;
       padding-right: 5px;
-      font-size: 2vh;
+      font-size: 3vh;
       background-color: rgba(0, 0, 0, .2);
     }
     #message img {
       max-width: 200px;
     }
     #input {
-      font-size: 2vh;
+      font-size: 3vh;
       background-color: rgba(0, 0, 0, .2);
       border: none;
       outline: none;
