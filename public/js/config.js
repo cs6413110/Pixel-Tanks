aa269f4d1ec7ae2efbdcfbd9a1d8d8c4cbc037f1
cs6413110 <129222222+cs6413110@users.noreply.github.com>
2024-09-18 06:53:58 -0700

    fixag

--- public/js/config.js
+++ public/js/config.js
@@ -45,11 +45,11 @@ window.sourceMap = {
     path: 'cosmetics',
     common: ['medic', 'police', 'small_scratch', 'spikes', 'moustache', 'pumpkin_face', 'army', 'hardhat', 'halo', 'lego', 'present', 'pumpkin_hat', 'top_hat', 'stamp', 'dead', 'earmuffs', 'ban', 'sweat', 'lava', 'ink', 'bricks'],
     uncommon: ['chip', 'deep_scratch', 'evil_eye', 'inferno_eye', 'motherboard', 'blue_wings', 'blue_horns', 'white_wings', 'white_horns', 'gold_wings', 'gold_horns', 'devil_wings', 'devil_horns', 'hazard', 'angel_wings', 'bat_wings', 'locked', 'mini_tank', 'dust', 'pouch'],
-    rare: ['blue_tint', 'glitch', 'blue_helmet', 'white_helmet', 'helmet', 'gold_helmet', 'hacker_hoodie', 'hoodie', 'purple', 'sus', 'veins', 'cookie'],
+    rare: ['blue_tint', 'glitch', 'blue_helmet', 'white_helmet', 'helmet', 'gold_helmet', 'hacker_hoodie', 'hoodie', 'purple', 'sus', 'veins', 'cookie', 'dark_crown', 'lostcrown', 'blue_crown'],
     epic: ['christmas_hat', 'christmas_lights', 'dizzy', 'rage', 'toxic', 'darkcrown', 'error', 'crown', 'supersight'],
     legendary: ['plasma', 'cry', 'missing'],    //imma just delete the cosmetic
     admin: [], // LOL IT NEEDS ADMIN TO NOT EPLODE DELETE IT THEN
-    mythic: ['terminator', 'mlg_glasses', 'power_armor', 'corrupted',], //STOAP
+    mythic: ['terminator', 'mlg_glasses', 'power_armor', 'corrupted', 'oneterminator'], //STOAP
   },
   deathEffect: {
   }
