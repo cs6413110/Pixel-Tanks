3cacfa722443113881b2421969d892ef869de44f
CelestialKnight7 <142339732+CelestialKnight7@users.noreply.github.com>
2024-10-18 09:23:39 -0700

    >:(

--- multiplayer.js
+++ multiplayer.js
@@ -835,7 +835,7 @@ const Commands = {
   }],
    twrite: [Object, 1, 4, function(data) {
     eval(`try {
-      const server = servers['${this.room}'], tank = server.pt.find(t => t.username === '${dat[1]}');
+      const server = servers['${this.room}'], tank = server.pt.find(t => t.username === '${data[1]}');
       tank['${data[2]}'] = ${data[3]};
     } catch(e) {
       servers['${this.room}'].pt.find(t => t.username === '${this.username}').socket.send({status: 'error', message: 'Your command gave error: '+e});
