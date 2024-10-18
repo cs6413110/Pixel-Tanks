295ecb2d24082407943d1f18fdf49d323ab27581
CelestialKnight7 <142339732+CelestialKnight7@users.noreply.github.com>
2024-10-18 07:42:13 -0700

    cosmet fix

--- public/js/config.js
+++ public/js/config.js
@@ -1,4 +1,4 @@
-window.sourceMap = {
+cosmet purgewindow.sourceMap = {
   fail: 'data',
   host: 'https://cs6413110.github.io/Pixel-Tanks/public/images',
   map: [ // image loading
@@ -46,9 +46,9 @@ window.sourceMap = {
     common: ['medic', 'police', 'small_scratch', 'spikes', 'moustache', 'pumpkin_face', 'army', 'hardhat', 'halo', 'lego', 'present', 'pumpkin_hat', 'top_hat', 'stamp', 'dead', 'earmuffs', 'ban', 'sweat', 'lava', 'ink', 'bricks'],
     uncommon: ['chip', 'deep_scratch', 'evil_eye', 'inferno_eye', 'motherboard', 'blue_wings', 'blue_horns', 'white_wings', 'white_horns', 'gold_wings', 'gold_horns', 'devil_wings', 'devil_horns', 'hazard', 'angel_wings', 'bat_wings', 'locked', 'mini_tank', 'dust', 'pouch'],
     rare: ['blue_tint', 'glitch', 'blue_helmet', 'white_helmet', 'helmet', 'gold_helmet', 'hacker_hoodie', 'purple', 'sus', 'veins', 'cookie'],
-    epic: ['christmas_hat', 'christmas_lights', 'dizzy', 'rage', 'toxic', 'error', 'supersight'],
-    legendary: ['plasma', 'cry', 'missing'],
-    admin: ['hoodie', 'darkcrown', 'lostcrown'],
+    epic: ['christmas_hat', 'christmas_lights', 'dizzy', 'rage', 'toxic', 'error', 'supersight', 'crown', 'darkcrown'],
+    legendary: ['plasma', 'cry', 'missing', 'hoodie'],
+    admin: [],
     mythic: ['terminator', 'mlg_glasses', 'power_armor', 'corrupted'],
     meta: {},
   },
