dca5d262d8baa677b6d0b026e7d7617b322d61b4
BreadSpammer <bradleydunham65@gmail.com>
2024-10-22 12:30:24 -0700

    :( 2

--- public/js/Client.js
+++ public/js/Client.js
@@ -748,7 +748,7 @@ class Client {
     }
     if (k === 69 && this.ip === null) {
       if (Engine.collision(this.tank.x, this.tank.y, 80, 80, 2000, 4300, 400, 300)) Menus.softTrigger('inventory');
-      if (Engine.collision(this.tank.x, this.tank.y, 80, 80, 1900, 3400, 600, 300)) Menus.softTrigger('crate');// now it is EZ
+      if (Engine.collision(this.tank.x, this.tank.y, 80, 80, 1900, 3400, 600, 300)) Menus.softTrigger('shop');// now it is EZ
     }
     if (k === PixelTanks.userData.keybinds.fire) {
       this.fire(0);
