f33dddf01fd4be6a6ed95677cc893ec61b5f4dd5
cs6413110 <129222222+cs6413110@users.noreply.github.com>
2024-10-23 08:32:42 -0700

    Optimize lines of code

--- public/js/Block.js
+++ public/js/Block.js
@@ -1,8 +1,5 @@
 class Block {
-  static args = ['x', 'y', 'type', 'team', 'host'];
-  static raw = ['x', 'y', 'type', 'team', 'maxHp', 'hp'];
-  static raw2 = ['x', 'y', 'type', 'team'];
-  static update = ['s', 'hp'];
+  static args = ['x', 'y', 'type', 'team', 'host'], raw = ['x', 'y', 'type', 'team', 'maxHp', 'hp'], raw2 = ['x', 'y', 'type', 'team'], update = ['s', 'hp'];
   constructor() {
     this.cells = new Set();
     this.t = []; // can be removed maybe?
@@ -11,20 +8,17 @@ class Block {
     this.id = host.genId(1);
     for (const i in Block.args) this[Block.args[i]] = arguments[i];
     this.maxHp = this.hp = [Infinity, Infinity, 300, 200, 100, 50][['void', 'barrier', 'gold', 'strong', 'weak', 'spike'].indexOf(type)];
-    if (!(this.c = type !== 'fire' && type !== 'airstrike' && type !== 'smoke')) this.sd = setTimeout(() => this.destroy(), type === 'fire' ? 2500 : (type === 'smoke' ? 10000 : 6000));
-    if (type === 'spike') this.c = false;
-    if (type === 'airstrike') for (let i = 0; i < 80; i++) this.t.push(setTimeout(() => this.host.d.push(A.template('Damage').init(this.x+Math.floor(Math.random()*250)-50, this.y+Math.floor(Math.random()*250)-50, 100, 100, 50, this.team, this.host)), 5000+Math.random()*500));
+    if (!(this.c = [‘fire’, ‘airstrike’, ‘smoke’, ‘spike’].includes(type))) this.sd = setTimeout(() => this.destroy(), type === 'fire' ? 2500 : (type === 'smoke' ? 10000 : 6000));
+    if (type === 'airstrike') for (let i = 0; i < 80; i++) this.t.push(setTimeout(() => this.host.d.push(A.template('Damage').init(this.x+Math.floor(Math.random()*250)-50, this.y+Math.floor(Math.random()*250)-50, 100, 100, 50, this.team, this.host)), 5000+Math.random()*500)); // airstrike rework needed
     if (type === 'smoke') for (let i = 0; i < 1600; i++) this.t.push(setTimeout(() => this.host.d.push(A.template('Damage').init(this.x+Math.floor(Math.random()*350)-150, this.y+Math.floor(Math.random()*350)-150, 200, 200, 0, this.team, this.host)), Math.random()*10000));
     if (this.c && this.x % 100 === 0 && this.y % 100 === 0 && this.x >= 0 && this.x <= 5900 && this.y >= 0 && this.y <= 5900) host.map.setWalkableAt(Engine.r(Math.floor(this.x/100)), Engine.r(Math.floor(this.y/100)), false);
     host.loadCells(this, this.x, this.y, 100, 100);
-    host.updateEntity(this, type === 'void' || type === 'barrier' ? Block.raw2 : Block.raw);
+    host.updateEntity(this, this.hp === Infinity ? Block.raw2 : Block.raw);
     return this;
   }
   damage(d, type) {
-    if (this.hp === Infinity) return;
-    this.s = Date.now();
-    if ((this.hp = Math.min(this.maxHp, this.hp-d)) <= 0) return this.destroy();
-    this.host.updateEntity(this, Block.update);
+    if (this.hp === Infinity) return; else this.s = Date.now();
+    if ((this.hp = Math.min(this.maxHp, this.hp-d)) <= 0) return this.destroy(); else this.host.updateEntity(this, Block.update);
   }
   reset() {
     for (const property of ['x', 'y', 'maxHp', 'hp', 'type', 'team', 's' ,'c', 'updatedLast', 'host']) this[property] = undefined;
