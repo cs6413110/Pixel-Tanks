9d7f7e376396804fc824c1ea8345054131b724b8
cs6413110 <129222222+cs6413110@users.noreply.github.com>
2024-10-24 12:36:29 -0700

    unexist big tank

--- public/js/PixelTanks.js
+++ public/js/PixelTanks.js
@@ -567,13 +567,14 @@ class PixelTanks {
           if (!PixelTanks.userData.perk[0] || (PixelTanks.userData.perk[0] === 'undefined')) GUI.drawImage(PixelTanks.images.menus.broke, 844, 816, 88, 88, 1);
           if (PixelTanks.userData.perk[1] && (PixelTanks.userData.perk[1] !== 'undefined')) GUI.drawImage(PixelTanks.images.menus[perkKey[Math.floor(PixelTanks.userData.perk[1])]], 932, 816, 88, 88, 1, 0, 0, 0, 0, undefined, ((PixelTanks.userData.perk[1]%1)*10-1)*40, 0, 40, 40);
           if (!PixelTanks.userData.perk[1] || (PixelTanks.userData.perk[1] === 'undefined')) GUI.drawImage(PixelTanks.images.menus.broke, 932, 816, 88, 88, 1);
-          PixelTanks.renderBottom(680, 380, 240, PixelTanks.userData.color);
+          /*PixelTanks.renderBottom(680, 380, 240, PixelTanks.userData.color);
           GUI.drawImage(PixelTanks.images.tanks.bottom, 680, 380, 240, 240, 1);
           PixelTanks.renderTop(680, 380, 240, PixelTanks.userData.color, (-Math.atan2(this.target.x, this.target.y)*180/Math.PI+360)%360);
           GUI.drawImage(PixelTanks.images.tanks.top, 680, 380, 240, 270, 1, 120, 120, 0, 0, (-Math.atan2(this.target.x, this.target.y)*180/Math.PI+360)%360);
           if (PixelTanks.userData.cosmetic_body !== 'undefined') PixelTanks.renderCosmetic(PixelTanks.images.cosmetics[PixelTanks.userData.cosmetic_body], 680, 380, 240, 270, (-Math.atan2(this.target.x, this.target.y)*180/Math.PI+360)%360);
           if (PixelTanks.userData.cosmetic !== 'undefined') PixelTanks.renderCosmetic(PixelTanks.images.cosmetics[PixelTanks.userData.cosmetic], 680, 380, 240, 270, (-Math.atan2(this.target.x, this.target.y)*180/Math.PI+360)%360);
           if (PixelTanks.userData.cosmetic_hat !== 'undefined') PixelTanks.renderCosmetic(PixelTanks.images.cosmetics[PixelTanks.userData.cosmetic_hat], 680, 380, 240, 270, (-Math.atan2(this.target.x, this.target.y)*180/Math.PI+360)%360);
+          */
           const key = {tactical: [7, 7], fire: [7, 61], medic: [7, 115], stealth: [61, 7], builder: [61, 61], warrior: [61, 115]};
           if (!PixelTanks.userData.class) PixelTanks.userData.class = 'undefined';
           if (!PixelTanks.userData.classes || !PixelTanks.userData.class || PixelTanks.userData.class === 'undefined') GUI.drawImage(PixelTanks.images.menus.broke, 1112, 816, 88, 88, 1);
