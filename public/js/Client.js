142225dde64a28af09b64e005143c053dbb96eca
cs6413110 <129222222+cs6413110@users.noreply.github.com>
2024-10-28 10:09:50 -0700

    check if idling is the problem

--- public/js/Client.js
+++ public/js/Client.js
@@ -85,12 +85,11 @@ class Client {
   getIdType = id => ['pt', 'b', 's', 'ai', 'd'][Math.floor(id)];
 
   moveDrones() {                        // 0   1   2   3  4  5  6    7    8    9     10    11
-    try {
     for (const drone of this.drones) { // [ox, oy, or, x, y, r, tx, ty, type, idle, scan, frame]
       const xd = drone[3]-drone[6], yd = drone[4]-drone[7], tr = Engine.toAngle(xd, yd)-90, d = Math.sqrt(yd**2+xd**2), a = d/2;
       const diff = (tr-drone[5]+360)%360, dir = diff < 180 ? 1 : -1;
       drone[5] = diff > 3 ? (drone[5]+dir*3+360)%360 : tr;
-      if (Math.random() < (drone[9] ? .04 : .02) && (drone[8] === 1 || d < 200)) drone[9] = !drone[9]; // idling % chance
+      if (Math.random() < (drone[9] ? .5 : .002) && (drone[8] === 1 || d < 200)) drone[9] = !drone[9]; // idling % chance
       if ((!drone[6] && !drone[7]) || !Math.sqrt((drone[3]-drone[6])**2+(drone[4]-drone[7])**2)) {
         drone[9] = true; // idle when reached destination
         drone[6] = Math.random() < .5 ? 70 : -70;
@@ -108,7 +107,6 @@ class Client {
       drone[4] -= yd/a;
       drone[11] = (drone[11]+.2)%3; 
     }
-    } catch(e) {alert(e)}
   }
 
   interpret(data) {
