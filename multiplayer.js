fb51737a72ae53fa57033136a18aee797dd0ae4d
cs6413110 <129222222+cs6413110@users.noreply.github.com>
2024-11-02 10:17:57 -0700

    Update multiplayer.js

--- multiplayer.js
+++ multiplayer.js
@@ -20,9 +20,9 @@ const token = fs.readFileSync('discord.json', 'utf8').replace(/\n/g, '');
 console.log(JSON.stringify(token));
 const channel = '1301321677220741180'; // temp, move to file
 client.on('messageCreate', m => {
-  if (m.channel.id !== channel || m.author.id === 'Pixel Tanks#4272') return;
+  if (m.channel.id !== channel || m.author.id === '1301716399999160392') return;
   
-  for (const server of Object.values(servers)) server.logs.push({m: '[DISCORD]['+m.author.username+', '+m.author.id+'] '+m.content, c: '#ffffff'});
+  for (const server of Object.values(servers)) server.logs.push({m: '[DISCORD]['+m.author.username+'] '+m.content, c: '#ffffff'});
 });
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
