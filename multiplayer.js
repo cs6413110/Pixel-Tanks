92369cb630a46d909e0c7c05ace621c5c4e944b5
cs6413110 <129222222+cs6413110@users.noreply.github.com>
2024-11-04 09:56:19 -0700

    Update multiplayer.js

--- multiplayer.js
+++ multiplayer.js
@@ -29,6 +29,7 @@ client.on('messageCreate', m => {
       toDiscord(d.m);
     }};
     if (!discordAcceptable.includes(data[0])) return toDiscord('Invalid command. Not in list: '+discordAcceptable);
+    toDiscord(data);
     Commands[data[0]](data, socket, {}, t, logs);
   }
   for (const server of Object.values(servers)) server.logs.push({m: '[DISCORD]['+m.author.username+'] '+m.content, c: '#ffffff'});
