fbf650cbebb9bdd0a4da3251215ed8fc5c695aea
BreadSpammer <bradleydunham65@gmail.com>
2024-11-05 16:48:18 -0700

    Update Engine.js

--- public/js/Engine.js
+++ public/js/Engine.js
@@ -208,7 +208,7 @@ class Engine {
 
   ondeath(t, m={}) {
     let core = Engine.hasPerk(t.perk, 7), x = t.x-99, y = t.y-99;
-    if (core) setTimeout(() => A.template('Damage').init(x, y, 298, 298, [0, 100, 300, 500][core], t.team, this), 500);
+    if (core) A.template('Damage').init(x, y, 298, 298, [0, 100, 300, 500][core], t.team, this)
     this.logs.push({m: this.deathMsg(t.username, m.username), c: (m.username === undefined ? '#FF8C00': (Engine.getTeam(m.team) === 'RED' ? '#FF0000' : (Engine.getTeam(m.team) === 'BLUE' ? '#0000FF' : '#FF8C00')))});
     if (t.privateLogs) t.privateLogs.push({m: this.tipMsg(t.username, m.username), c: '#80FFF9'});
     for (let i = this.ai.length-1; i >= 0; i--) if (Engine.getUsername(this.ai[i].team) === t.username) this.ai[i].destroy();
