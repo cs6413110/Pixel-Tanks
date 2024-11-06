52d09f72e47737a1371e91340ad77395db64cd48
3foe <127255188+3foe@users.noreply.github.com>
2024-11-06 07:03:24 -0700

    fix da bread

--- public/js/Tank.js
+++ public/js/Tank.js
@@ -111,7 +111,7 @@ class Tank {
     this.damageTimeout = setTimeout(() => {this.damage = false}, 1000);
     this.damage = {d: (this.damage ? this.damage.d : 0)+a, x, y};
     let core = Engine.hasPerk(this.perk, 9), shield = Engine.hasPerk(this.perk, 1), boom = Engine.hasPerk(this.perk, 7);
-    if (this.hp <= 1000 && this.host.ondeath) if (!core || Math.random() > 0.50) {
+    if (this.hp <= 0 && this.host.ondeath) if (!core || Math.random() > 0.50) {
       if (boom) this.immune = true;
       return setTimeout(() => this.host.ondeath(this, this.host.pt.concat(this.host.ai).find(t => t.username === u)), (boom) ? 1000 : 0);
     } else this.core = Date.now();
