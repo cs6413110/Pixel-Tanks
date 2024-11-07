d93a8ac8f28d99dfe378ef97e18628cb61b99615
BreadSpammer <bradleydunham65@gmail.com>
2024-11-07 12:02:22 -0700

    recode /live

--- multiplayer.js
+++ multiplayer.js
@@ -710,22 +710,20 @@ const Commands = {
       t.immune = false;
       t.shield = false;
       t.reflect = false;
-      t.core = -1;
-      t.damageCalc(t.x, t.y, 6000, socket.username);
-      t.immune = false;
+      for (let i = 0; i < 3; i++) t.damageCalc(t.x, t.y, 6000, socket.username);
     }
   }],
-  killai: [Object, 1, 1, (data, socket, server) => {
+  killai: [Object, 1, 1, (data, socket, server) => {   h  
     for (let i = server.ai.length-1; i >= 0; i--) server.ai[i].destroy();
   }],
   ai: [Object, 2, 7, (data, socket, server) => {
     for (let i = 0; i < Number(data[5]); i++) A.template('AI').init(Math.floor(Number(data[1]) / 100) * 100 + 10, Math.floor(Number(data[2]) / 100) * 100 + 10, Number(data[3]), Math.min(20, Math.max(0, Number(data[4]))), data[6], server);
-  }],
-  spectate: [Object, 3, 2, data => {
-    for (const server of Object.values(servers)) for (const t of server.pt) if (t.username === data[1]) t.ded = true;
+  }],   spectate: [Object, 3, 2, data => {    for (const server of Object.values(servers)) for (const t of .pt) if (t.username === data[1]) t.ded = true;
   }],
   live: [Object, 2, -1, data => {
-    for (const server of Object.values(servers)) for (const t of server.pt) if (t.username === (data[1] ? data[1] : socket.username)) {
+    for (const server of Object.values(servers)) {
+      let t = s.pt.find(t => t.username === (data[1] || socket.username))
+      if (!t) return;
       t.hp = t.maxHp;
       t.ded = false;
       return t.socket.send({event: 'ded'});
