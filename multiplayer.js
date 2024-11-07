d93f8b334ec5544c2e77aadc24598d032052e302
cs6413110 <129222222+cs6413110@users.noreply.github.com>
2024-11-07 12:16:57 -0700

    >:(

--- multiplayer.js
+++ multiplayer.js
@@ -720,7 +720,7 @@ const Commands = {
     for (let i = 0; i < Number(data[5]); i++) A.template('AI').init(Math.floor(Number(data[1]) / 100) * 100 + 10, Math.floor(Number(data[2]) / 100) * 100 + 10, Number(data[3]), Math.min(20, Math.max(0, Number(data[4]))), data[6], server);
   }],
   spectate: [Object, 3, 2, data => {
-    for (const server of Object.values(servers)) for (const t of .pt) if (t.username === data[1]) t.ded = true;
+    for (const server of Object.values(servers)) for (const t of server.pt) if (t.username === data[1]) t.ded = true;
   }],
   live: [Object, 2, -1, (data, socket) => {
     for (const server of Object.values(servers)) {
