7b00899777b7d4aaf0b643c8572cb2451c3eb16a
cs6413110 <129222222+cs6413110@users.noreply.github.com>
2024-11-15 10:08:00 -0700

    ez

--- public/js/Engine.js
+++ public/js/Engine.js
@@ -119,8 +119,9 @@ class Engine {
     } else if (a.includes('airstrike')) {
       const rotation = Math.floor(Math.random()*360), h = a.replace('airstrike', '').split('x');
       for (let i = -2; i <= 2; i++) {
-        const airstrike = A.template('Block').init(Number(h[0])+i*Math.cos(Math.PI*rotation/180)*100, Number(h[1])+i*Math.sin(Math.PI*rotation/180)*100, 'airstrike', Engine.parseTeamExtras(t.team), this);
+        const airstrike = A.template('Block');
         airstrike.timer = i/2+5;
+        airstrike.init(Number(h[0])+i*Math.cos(Math.PI*rotation/180)*100, Number(h[1])+i*Math.sin(Math.PI*rotation/180)*100, 'airstrike', Engine.parseTeamExtras(t.team), this);
         this.b.push(airstrike);
       }
     } else if (a.includes('flashbang')) {
