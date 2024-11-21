00b720d34bbb02e926cd780481fe56fb416a4534
cs6413110 <129222222+cs6413110@users.noreply.github.com>
2024-11-21 10:08:41 -0700

    make spike not despawn

--- public/js/Block.js
+++ public/js/Block.js
@@ -11,7 +11,7 @@ class Block {
     this.id = host.genId(1);
     for (const i in Block.args) this[Block.args[i]] = arguments[i];
     this.maxHp = this.hp = [Infinity, Infinity, 300, 200, 100, 50][['void', 'barrier', 'gold', 'strong', 'weak', 'spike'].indexOf(type)];
-    if (!(this.c = !['fire', 'airstrike', 'smoke', 'spike'].includes(type))) this.sd = setTimeout(() => this.destroy(), type === 'fire' ? 2500 : (type === 'smoke' ? 10000 : 6000));
+    if (!(this.c = !['fire', 'airstrike', 'smoke', 'spike'].includes(type))) if (type !== 'spike') this.sd = setTimeout(() => this.destroy(), type === 'fire' ? 2500 : (type === 'smoke' ? 10000 : 6000));
     if (type === 'airstrike') for (let i = 0; i < 5; i++) this.t.push(setTimeout(() => this.host.d.push(A.template('Damage').init(this.x+[0, 100, 0, 100, 50][i], this.y+[0, 0, 100, 100, 50][i], 150, 150, 200, this.team, this.host)), this.timer*1000+Math.random()*200));
     if (type === 'smoke') for (let i = 0; i < 1600; i++) this.t.push(setTimeout(() => this.host.d.push(A.template('Damage').init(this.x+Math.floor(Math.random()*350)-150, this.y+Math.floor(Math.random()*350)-150, 200, 200, 0, this.team, this.host)), Math.random()*10000));
     if (this.c && this.x % 100 === 0 && this.y % 100 === 0 && this.x >= 0 && this.x <= 5900 && this.y >= 0 && this.y <= 5900) host.map.setWalkableAt(Engine.r(Math.floor(this.x/100)), Engine.r(Math.floor(this.y/100)), false);
