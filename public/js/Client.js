203d07719f8a1a6dcb226824ec4c85703059dad8
cs6413110 <129222222+cs6413110@users.noreply.github.com>
2024-11-26 10:57:18 -0700

    AAAAAAA

--- public/js/Client.js
+++ public/js/Client.js
@@ -577,7 +577,7 @@ class Client {
       GUI.draw.fillRect([308, 408, 1120, 1196, 1272][i], 952+Math.min((Date.now()-this.timers[type].time)/this.timers[type].cooldown, 1)*48, 48, 48);
     }
     GUI.drawText(this.dedTime < Date.now()-10000 ? 'Hit F to Respawn' : this.hostupdate?.global || '', 800, 30, 60, '#ffffff', .5);
-    GUI.drawText('TPS: '+(this.hostupdate?.tickspeed || ''), 200, 30, 30 '#ffffff', 0);
+    GUI.drawText('TPS: '+(this.hostupdate?.tickspeed || ''), 200, 30, 30, '#ffffff', 0);
     
     if (this.debugMode) {// 0 = disabled, 1 = ping, 2 = fps, 3 = ops, 4 = ups
       const infoset = [null, this.pings, this.fps, this.ops, this.ups][this.debugMode];
