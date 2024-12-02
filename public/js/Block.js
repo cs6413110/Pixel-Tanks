bcb2af981a4406176ef6016758523c4f5c970cff
cs6413110 <129222222+cs6413110@users.noreply.github.com>
2024-12-02 10:32:56 -0700

    Update Block.js

--- public/js/Block.js
+++ public/js/Block.js
@@ -11,8 +11,7 @@ class Block {
     this.id = host.genId(1);
     for (const i in Block.args) this[Block.args[i]] = arguments[i];
     this.maxHp = this.hp = [300, 200, 100, 50, 200][['gold', 'strong', 'weak', 'spike', 'crate'].indexOf(type)] || Infinity;
-    if (type === 'airstrike') this.host.logs.push({m: this.time+':'+Math.abs(this.timer), c: '#ffffff'});
-    if (!(this.c = !['fire', 'airstrike', 'smoke', 'spike'].includes(type))) if (type !== 'spike') this.sd = setTimeout(() => this.destroy(), type === 'fire' ? 2500 : (type === 'smoke' ? 10000 : Math.abs(this.timer)*1000));
+    if (!(this.c = !['fire', 'airstrike', 'smoke', 'spike'].includes(type))) if (type !== 'spike') this.sd = setTimeout(() => this.destroy(), type === 'fire' ? 2500 : (type === 'smoke' ? 10000 : Math.abs(this.timer)*1000+200));
     if (type === 'airstrike' && this.timer > 0) for (let i = 0; i < 5; i++) this.t.push(setTimeout(() => this.host.d.push(A.template('Damage').init(this.x+[0, 100, 0, 100, 50][i], this.y+[0, 0, 100, 100, 50][i], 150, 150, 200, this.team, this.host)), this.timer*1000+Math.random()*200));
     if (type === 'airstrike' && this.timer < 0) this.t.push(setTimeout(() => this.host.b.push(A.template('Block').init(this.x+50, this.y+50, 'crate', this.team, this.host)), 5000));
     if (type === 'smoke') for (let i = 0; i < 1600; i++) this.t.push(setTimeout(() => this.host.d.push(A.template('Damage').init(this.x+Math.floor(Math.random()*350)-150, this.y+Math.floor(Math.random()*350)-150, 200, 200, 0, this.team, this.host)), Math.random()*10000));
