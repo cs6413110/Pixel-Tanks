906843f6805ffc6c6f85066e665f85cb89e63a51
cs6413110 <129222222+cs6413110@users.noreply.github.com>
2024-12-05 09:33:12 -0700

    Postpone level generation off Engine construction

--- public/js/Engine.js
+++ public/js/Engine.js
@@ -17,7 +17,7 @@ class Engine {
       for (let x = 0; x < 60; x++) this.cells[y][x] = new Set();
     }
     this.map = new PF.Grid(60, 60);
-    this.levelReader(levels[Math.floor(Math.random()*levels.length)]);
+    setTimeout(() => this.levelReader(levels[Math.floor(Math.random()*levels.length)]));
     this.i.push(setInterval(() => this.tick(), 1000/60));
   }
 
