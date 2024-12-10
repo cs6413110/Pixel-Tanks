367e9882f7d593c90942503a123f239dcca3b953
cs6413110 <129222222+cs6413110@users.noreply.github.com>
2024-12-10 12:58:47 -0700

    UGH

--- public/js/Client.js
+++ public/js/Client.js
@@ -554,7 +554,7 @@ class Client {
     if (!this.multiplayer) {
       const sample = {...this.world.pt[0]};
       sample.host = undefined; // prevent loops
-      document.getElementById('viewport').innerHTML = 's='+sample.ded+' p='+player.ded+' t.h='+t[0].ded+' h='+this.world.pt[0].ded+' r='+JSON.stringify(player.raw);
+      document.getElementById('viewport').innerHTML = 's='+sample.ded+' p='+player.ded+' t.h='+t[0].ded+' h='+this.world.pt[0].ded+' r='+player.raw.ded;
       document.getElementById('viewport').style.color = '#ffffff';
     }
     
