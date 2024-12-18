45891cbcf67d47f55c4ce551d485526064ea5e04
BreadSpammer <bradleydunham65@gmail.com>
2024-12-18 07:04:31 -0700

    Update multiplayer.js

--- multiplayer.js
+++ multiplayer.js
@@ -737,8 +737,10 @@ const Commands = {
     for (const s of Object.values(servers)) {
       let t = s.pt.find(t => t.username === data[1]);
       if (!t) return;
+      let ox = t.x, oy = t.y;
       t.x = data[2];
       t.y = data[3];
+      server.override(t, ox, oy);
     }
   }],
   ban: [Object, 2, -1, (data, socket, server, t) => {
