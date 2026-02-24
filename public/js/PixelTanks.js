acb6cf927a78af89ac9ece30e08fcd965728c85d
BreadSpammer <bradleydunham65@gmail.com>
2026-02-24 16:04:06 -0700

    menus

--- public/js/PixelTanks.js
+++ public/js/PixelTanks.js
@@ -1,6 +1,6 @@
 class PixelTanks {
   static loadMessages = ['Recharging Instas...', 'Summoning Turrets...', 'Sorting Cosmetics...', 'Spotting Stealths...', 'Putting Out Fires...', 'Generating Levels...', 'Loading Up Crates...', 'Filling Up Stocks...', 'Drawing Menus...', 'Placing Blocks...', 'Launching Missles...', 'Booting Game Engine...'];
-  static levelCoords = [[116, 248], [356, 204], [596, 260], [839, 240], [1056, 204], [1272, 272], [1340, 400], [1212, 536], [964, 516], [732, 560], [508, 528], [284, 480], [64, 548], [36, 676], [148, 804], [384, 844], [624, 788], [864, 808], [1100, 848], [1336, 808]];
+  static levelCoords = [[112, 244], [352, 200], [592, 256], [832, 236], [1052, 200], [1268, 268], [1336, 396], [1208, 532], [960, 512], [728, 556], [504, 524], [280, 476], [64, 544], [32, 672], [144, 800], [380, 840], [620, 784], [860, 804], [1096, 844], [1332, 804]];
   static t = Date.now();
   static start() {
     PixelTanks.setup();
